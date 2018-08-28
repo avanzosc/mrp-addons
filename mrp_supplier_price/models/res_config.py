b4aeb098afd4bf303b79d789b1d92104d5457cb5
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-08-28 13:22:11 +0200

    [MIG] mrp_supplier_price: Migration to 11.0 * add more fields from profit to view * clean view and update translations * new fields for purchases * Fix README

--- mrp_supplier_price/models/res_config.py
+++ mrp_supplier_price/models/res_config.py
@@ -1,43 +1,35 @@
-# -*- coding: utf-8 -*-
-# © 2016 Oihane Crucelaegui - AvanzOSC
+# Copyright 2016 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from openerp import api, fields, models
 
 
 class MrpConfigSettings(models.TransientModel):
-    _inherit = 'mrp.config.settings'
+    _inherit = 'res.config.settings'
 
     subtotal_by_unit = fields.Boolean(
         string='Compute unitary total values',
         help='This will allow you to define if the total of the scheduled '
         'product list is computed by unit or not.')
+    group_show_unitary_info = fields.Boolean(
+        string="Show Unitary Info in Scheduled Lines",
+        implied_group="mrp_supplier_price.group_show_unitary_info")
 
-    def _get_parameter(self, key, default=False):
-        param_obj = self.env['ir.config_parameter']
-        rec = param_obj.search([('key', '=', key)])
-        return rec or default
-
-    def _write_or_create_param(self, key, value):
-        param_obj = self.env['ir.config_parameter']
-        rec = self._get_parameter(key)
-        if rec:
-            if not value:
-                rec.unlink()
-            else:
-                rec.value = value
-        elif value:
-            param_obj.create({'key': key, 'value': value})
+    @api.model
+    def get_values(self):
+        res = super(MrpConfigSettings, self).get_values()
+        get_param = self.env['ir.config_parameter'].sudo().get_param
+        # the value of the parameter is a nonempty string
+        res.update(
+            subtotal_by_unit=get_param('mrp.subtotal_by_unit',
+                                       'False').lower() == 'true',
+        )
+        return res
 
     @api.multi
-    def get_default_parameters(self):
-        def get_value(key, default=''):
-            rec = self._get_parameter(key)
-            return rec and rec.value or default
-        return {
-            'subtotal_by_unit': get_value('subtotal.by.unit', False),
-        }
-
-    @api.multi
-    def set_parameters(self):
-        self._write_or_create_param('subtotal.by.unit', self.subtotal_by_unit)
+    def set_values(self):
+        super(MrpConfigSettings, self).set_values()
+        set_param = self.env['ir.config_parameter'].sudo().set_param
+        # we store the repr of the values, since the value of the parameter is
+        # a required string
+        set_param('mrp.subtotal_by_unit', repr(self.subtotal_by_unit))
