197e33013af9cba0cc564d8aa06bf41e25f07319
Oihane Crucelaegui <oihanecruce@gmail.com>
2018-09-07 11:24:07 +0200

    [REM] delete mrp_production_editable_scheduled_products

--- mrp_production_editable_scheduled_products/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright 2014 Daniel Campos - AvanzOSC
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-from . import models
--- mrp_production_editable_scheduled_products/tests/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright 2015 Oihane Crucelaegui - AvanzOSC
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-from . import test_mrp_production_editable_scheduled_product
