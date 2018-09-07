d006da40ff6a72b86ee31615d87bb495289f2972
oihane <oihanecruce@gmail.com>
2018-09-07 12:09:47 +0200

    [REM] delete mrp_production_editable_scheduled_products (#16)

--- mrp_supplier_price/__manifest__.py
+++ mrp_supplier_price/__manifest__.py
@@ -2,11 +2,11 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Supplier Price in Scheduled Manufacturing Products",
-    "version": "11.0.1.0.0",
+    "version": "11.0.2.0.0",
     "license": "AGPL-3",
     "depends": [
         "mrp",
-        "mrp_production_editable_scheduled_products",
+        "mrp_scheduled_products",
     ],
     "author": "AvanzOSC",
     "website": "http://www.avanzosc.es",
