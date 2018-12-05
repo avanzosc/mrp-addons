983460e96bafa0f449bcd5eeffa6c39bb36e948c
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2018-12-05 14:57:22 +0100

    [ADD] mrp_production_fix_out_move: No duplicate stock picking move lines from production.

---
+++ mrp_production_fix_out_move/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "MRP Production Fix Out Move",
+    "summary": "Manufacturing",
+    "version": "8.0.1.0.0",
+    "category": "Manufacturing",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "contributors": [
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+        "Alfredo de la Fuente <alfredodelafuente@avanzosc.es>",
+    ],
+    "depends": [
+        "sale_stock",
+        "mrp",
+    ],
+    "data": [
+    ],
+    "installable": True,
+}
