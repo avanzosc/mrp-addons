878e7f746d77f0ce9ae1050b684d71516c57bd72
mikelarre <mikelarregi@avanzosc.es>
2019-02-20 16:00:00 +0100

    [MIG] mrp_production_generate_structure

---
+++ mrp_production_generate_structure/models/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2018 Alfredo de la Fuente - Eider Oyarbide - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from . import mrp_production
+from . import purchase_order
