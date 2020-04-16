92bcb7988d4bc9b96cb6fe945b9e8bc1442cc2e3
Thor <alfredodelafuente@avanzosc.es>
2020-04-16 16:18:16 +0200

    [ADD] mrp_usability: On new shortcut in manufacturing orders form, to moves to consume.

---
+++ mrp_usability/tests/test_mrp_usability.py
@@ -0,0 +1,23 @@
+# Copyright 2020 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo.tests import common
+
+
+@common.at_install(False)
+@common.post_install(True)
+class TestMrpUsability(common.SavepointCase):
+    @classmethod
+    def setUpClass(cls):
+        super(TestMrpUsability, cls).setUpClass()
+        cls.production_obj = cls.env['mrp.production']
+
+    def test_mrp_usability(self):
+        productions = self.production_obj.search([])
+        for production in productions:
+            self.assertEquals(
+                production.moves_to_consume_count,
+                len(production.move_raw_ids))
+            res = production.button_show_moves_to_consume()
+            domain = [('id', 'in', production.move_raw_ids.ids)]
+            self.assertEquals(domain, res.get('domain', False))
