67d7ae54a0518678874476985e4496a6adb90453
Mikel Arregi <mikelarregi@avanzosc.es>
2020-10-13 15:50:21 +0200

    [FIX] sale_order_supplier_price: PEP8

--- sale_order_supplier_price/models/mrp_production_product_line.py
+++ sale_order_supplier_price/models/mrp_production_product_line.py
@@ -1,6 +1,6 @@
 # Copyright 2020 Mikel Arregi Etxaniz - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import api, fields, models, _
+from odoo import api, fields, models
 from odoo.addons import decimal_precision as dp
 
 
