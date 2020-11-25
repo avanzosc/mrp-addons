c1b8032908d92e0c7e47498debf5dc516dbd4eaa
Mikel Arregi <mikelarregi@avanzosc.es>
2020-11-25 15:38:52 +0100

    [IMP] version modules security changes

--- product_variant_custom_sale/__manifest__.py
+++ product_variant_custom_sale/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "Sale Product Version",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "license": "AGPL-3",
     "depends": [
         "sale", "product_variant_custom",
