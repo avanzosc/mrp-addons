e92ba25208fb9f536a346dd2411245904d813db1
Mikel Arregi <mikelarregi@avanzosc.es>
2020-12-17 17:19:36 +0100

    [fix] mrp_workorder_auto_final_lot: change name in __manifest__

--- mrp_workorder_auto_final_lot/__manifest__.py
+++ mrp_workorder_auto_final_lot/__manifest__.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Mikel Arregi Etxaniz - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
-    "name": "Workorder moves in view",
+    "name": "Auto final lot",
     "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "depends": [
