90a50b7e67946de7cd4fe6e57f42e04b449c7b64
Mikel Arregi <mikelarregi@avanzosc.es>
2021-02-15 17:12:39 +0100

    [ADD] New module mrp_workorder_grouping_by_material

---
+++ mrp_workorder_grouping_by_material/models/stock_production_lot.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import api, fields, models
+
+
+class StockProductionLot(models.Model):
+    _inherit = "stock.production.lot"
+
+    product_life_alert = fields.Boolean(
+        "Product Life Alert",
+        compute="_compute_product_life_alert"
+    )
+
+    @api.depends('life_date')
+    def _compute_product_life_alert(self):
+        current_date = fields.Datetime.now()
+        for lot in self:
+            lot.product_life_alert = lot.alert_date and lot.alert_date <= \
+                current_date or False
