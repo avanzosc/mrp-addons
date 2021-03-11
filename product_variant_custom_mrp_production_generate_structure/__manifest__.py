394cfe07b85ae3d5885f3c16da6a2ad0aca723f3
Mikel Arregi <mikelarregi@avanzosc.es>
2021-03-11 18:40:52 +0100

    [ADD] product_variant_custom_mrp_production_generate_structure

---
+++ product_variant_custom_mrp_production_generate_structure/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Glue module mrp_production_generate_structure and version modules",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "mrp_production_custom_variants_inherited_attributes",
+        "mrp_production_generate_structure",
+    ],
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Tools",
+    "data": [
+
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': True,
+}
