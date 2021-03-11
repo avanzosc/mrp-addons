c8844fe6cf4f9e9b6b29271511d1bd565acb2378
Mikel Arregi <mikelarregi@avanzosc.es>
2021-03-11 18:34:14 +0100

    [ADD] product_attribute_value_hide_is_custom

---
+++ product_attribute_value_hide_is_custom/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Glue module for sale and version modules",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "sale", "product_variant_custom"
+    ],
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Tools",
+    "data": [
+        "views/product_attribute_view.xml",
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': True,
+}
