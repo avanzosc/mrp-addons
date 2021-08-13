ded0d36a45792bb82ee701bc95194eb5aae7422d
odoo <mikelarregi@avanzosc.es>
2021-08-13 15:07:32 +0200

    [ADD] mrp_workorder_data_worksheet_header

---
+++ mrp_workorder_data_worksheet_header/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2021 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Workorder data in worksheet header",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [
+        "mrp", "mrp_workorder_grouping_by_material",
+    ],
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "category": "Manufacturing",
+    "data": [
+        "views/mrp_workorder_view.xml",
+        "report/mrp_workorder_report.xml",
+        "wizard/binary_container_view.xml",
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'external_dependencies': {"python": ["PyPDF2"]},
+}
