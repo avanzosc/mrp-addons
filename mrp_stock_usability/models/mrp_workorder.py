5d0d58637a03bb1f7d81be655f559b5bbc7a0dab
Oihane Crucelaegui <oihanecruce@gmail.com>
2021-10-27 10:49:34 +0200

    [IMP] mrp_stock_usability: black, isort, prettier

--- mrp_stock_usability/models/mrp_workorder.py
+++ mrp_stock_usability/models/mrp_workorder.py
@@ -9,16 +9,18 @@ class MrpWorkorder(models.Model):
     _inherit = "mrp.workorder"
 
     show_final_lots = fields.Boolean(
-        string="Show Final Lots",
-        compute="_compute_show_lots")
+        string="Show Final Lots", compute="_compute_show_lots"
+    )
     unreserve_visible = fields.Boolean(
         string="Allowed to Unreserve Inventory",
         compute="_compute_unreserve_visible",
-        help="Technical field to check when we can unreserve")
+        help="Technical field to check when we can unreserve",
+    )
     show_check_availability = fields.Boolean(
         compute="_compute_show_check_availability",
         help="Technical field used to compute whether the check availability "
-             "button should be shown.")
+        "button should be shown.",
+    )
 
     @api.depends("product_id.tracking")
     def _compute_show_lots(self):
@@ -29,12 +31,14 @@ class MrpWorkorder(models.Model):
     def _compute_unreserve_visible(self):
         for order in self:
             pending_raw_moves = order.move_raw_ids.filtered(
-                lambda m: m.state not in ("done", "cancel"))
+                lambda m: m.state not in ("done", "cancel")
+            )
             order.unreserve_visible = any(
-                [m.unreserve_visible for m in pending_raw_moves])
+                [m.unreserve_visible for m in pending_raw_moves]
+            )
 
     def _compute_show_check_availability(self):
-        """ According to `workorder.show_check_availability`, the
+        """According to `workorder.show_check_availability`, the
         "check availability" button will be displayed in the form view of a
         work order.
         """
@@ -44,8 +48,11 @@ class MrpWorkorder(models.Model):
                 continue
             workorder.show_check_availability = any(
                 move.state in ("waiting", "confirmed", "partially_available")
-                and float_compare(move.product_uom_qty, 0,
-                                  precision_rounding=move.product_uom.rounding)
+                and float_compare(
+                    move.product_uom_qty,
+                    0,
+                    precision_rounding=move.product_uom.rounding,
+                )
                 for move in workorder.move_raw_ids
             )
 
@@ -58,7 +65,8 @@ class MrpWorkorder(models.Model):
     def do_unreserve(self):
         for order in self:
             order.move_raw_ids.filtered(
-                lambda x: x.state not in ("done", "cancel"))._do_unreserve()
+                lambda x: x.state not in ("done", "cancel")
+            )._do_unreserve()
             order._refresh_wo_lines()
         return True
 
--- mrp_stock_usability/models/stock_move.py
+++ mrp_stock_usability/models/stock_move.py
@@ -8,23 +8,25 @@ class StockMove(models.Model):
     _inherit = "stock.move"
 
     unreserve_visible = fields.Boolean(
-        string='Allowed to Unreserve Inventory',
-        compute='_compute_unreserve_visible',
-        help='Technical field to check when we can unreserve')
+        string="Allowed to Unreserve Inventory",
+        compute="_compute_unreserve_visible",
+        help="Technical field to check when we can unreserve",
+    )
 
-    @api.depends('raw_material_production_id',
-                 'raw_material_production_id.is_locked',
-                 'raw_material_production_id.state',
-                 'quantity_done')
+    @api.depends(
+        "raw_material_production_id",
+        "raw_material_production_id.is_locked",
+        "raw_material_production_id.state",
+        "quantity_done",
+    )
     def _compute_unreserve_visible(self):
         for move in self:
             raw_production = move.raw_material_production_id
             already_reserved = (
-                (raw_production.is_locked and
-                 raw_production.state not in ('done', 'cancel')) and
-                move.move_line_ids)
-            move.unreserve_visible = (
-                not move.quantity_done > 0 and already_reserved)
+                raw_production.is_locked
+                and raw_production.state not in ("done", "cancel")
+            ) and move.move_line_ids
+            move.unreserve_visible = not move.quantity_done > 0 and already_reserved
 
     def button_assign(self):
         self.ensure_one()
