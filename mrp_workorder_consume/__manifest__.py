9a8b6791c39fcb08fa19b10a3cc4397ae742f431
oihane <oihanecruce@gmail.com>
2021-11-15 09:41:56 +0100

    [ADD] New module mrp_workorder_consume (#106)

---
+++ mrp_workorder_consume/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "MRP Work Order Consume",
+    "version": "13.0.1.0.0",
+    "category": "Manufacturing/Manufacturing",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "mrp",
+    ],
+    "excludes": [],
+    "data": [
+        "views/mrp_workorder_views.xml",
+    ],
+    "installable": True,
+}
