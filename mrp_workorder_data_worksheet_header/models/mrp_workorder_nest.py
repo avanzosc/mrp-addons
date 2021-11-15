f2fd8f32f8b45cd5d8ed0b7a39d4577fc05b3d77
oihane <oihanecruce@gmail.com>
2021-11-15 09:40:39 +0100

    [IMP] mrp_workorder_data_worksheet_header: use pdf viewer wizard (#107)

--- mrp_workorder_data_worksheet_header/models/mrp_workorder_nest.py
+++ mrp_workorder_data_worksheet_header/models/mrp_workorder_nest.py
@@ -7,27 +7,8 @@ from odoo import models
 class MrpWorkorderNest(models.Model):
     _inherit = "mrp.workorder.nest"
 
-    def show_worksheets(self):
-        self.ensure_one()
-        pdf = self.mapped("nested_line_ids").print_report()
-        if not pdf and self.worksheets:
-            return super().show_worksheets()
-        wizard = self.env["binary.container"].create({
-            "binary_field": pdf,
-        })
-        view_ref = self.env["ir.model.data"].get_object_reference(
-            "mrp_workorder_grouping_by_material",
-            "binary_container_view")
-        view_id = view_ref and view_ref[1] or False,
-        return {
-            "name": "Worksheet",
-            "domain": [],
-            "res_model": "binary.container",
-            "res_id": wizard.id,
-            "type": "ir.actions.act_window",
-            "view_mode": "form",
-            "view_type": "form",
-            "view_id": view_id,
-            "context": {},
-            "target": "new",
-        }
+    def get_worksheets(self):
+        pdf = self.mapped("nested_line_ids").get_worksheets()
+        if not pdf or not self.env.context.get("print", False):
+            return super().get_worksheets()
+        return pdf
