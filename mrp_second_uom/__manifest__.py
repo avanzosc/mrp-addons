02729beaf9082fc9dad82c8ab095464554653fb7
Tu Nombre <you@example.com>
2021-11-24 12:18:51 +0100

    [ADD] mrp_second_uom: Second UOM, factor and factor inverse in product.

---
+++ mrp_second_uom/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "MRP Second UOM",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "website": "http://www.avanzosc.es",
+    "category": "Product",
+    "depends": [
+        "product_second_uom",
+        "mrp"
+    ],
+    "data": [
+        "views/mrp_bom_views.xml",
+        "views/mrp_production_views.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
