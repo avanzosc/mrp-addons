0073e3210e49a9463916824fb2480f394635fabc
Tu Nombre <you@example.com>
2021-12-03 10:47:30 +0100

    [ADD] mrp_production_workorder_button: In MRP production, the button that creates the work orders invisible until all the products are available and reserved.

---
+++ mrp_production_workorder_button/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2020 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "MRP Production Workorder Button",
+    "version": "12.0.1.0.0",
+    "author": "AvanzOSC",
+    "license": "AGPL-3",
+    "category": "Manufacturing",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "mrp",
+    ],
+    "data": [
+        "views/mrp_production_views.xml",
+    ],
+    "installable": True
+}
