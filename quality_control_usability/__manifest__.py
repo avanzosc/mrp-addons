b991113753a4d92b9ecfe01176a9d5b72d1d5830
Tu Nombre <you@example.com>
2021-12-14 12:09:26 +0100

    [ADD] quality_control_usability: Make the success field editable.

---
+++ quality_control_usability/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Quality Control Usability",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "website": "https://www.avanzosc.es",
+    "category": "MRP",
+    "depends": [
+        "quality_control_oca",
+    ],
+    "data": [
+        "views/qc_inspection_views.xml",
+
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
