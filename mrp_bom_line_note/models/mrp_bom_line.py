b31124e3480c8d303800385721489a203ca0e541
Tu Nombre <you@example.com>
2021-12-16 15:02:01 +0100

    [ADD] mrp_bom_line_note: Note field in MRP BOM line.

---
+++ mrp_bom_line_note/models/mrp_bom_line.py
@@ -0,0 +1,9 @@
+# Copyright 2021 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models, fields
+
+
+class MrpBomLine(models.Model):
+    _inherit = "mrp.bom.line"
+
+    note = fields.Text(string='Note')
