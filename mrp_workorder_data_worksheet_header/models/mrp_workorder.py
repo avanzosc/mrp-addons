fe51e428500f9da541c13ad5dc42b2cae0b23c51
oihane <oihanecruce@gmail.com>
2021-12-20 11:33:48 +0100

    [IMP] mrp_workorder_data_worksheet_header: one header for all (#122)

--- mrp_workorder_data_worksheet_header/models/mrp_workorder.py
+++ mrp_workorder_data_worksheet_header/models/mrp_workorder.py
@@ -1,15 +1,69 @@
 # Copyright 2021 Mikel Arregi Etxaniz - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from odoo import _, models
+from base64 import b64decode, b64encode
+from io import BytesIO
+from logging import getLogger
+
+logger = getLogger(__name__)
+
+try:
+    # we need this to be sure PIL has loaded PDF support
+    from PIL import PdfImagePlugin  # noqa: F401
+except ImportError:
+    pass
+try:
+    from PyPDF2 import PdfFileWriter, PdfFileReader  # pylint: disable=W0404
+except ImportError:
+    logger.debug("Can not import PyPDF2")
 
 
 class MrpWorkorder(models.Model):
     _inherit = "mrp.workorder"
 
+    def print_report(self):
+        records = self.filtered(lambda r: r.worksheet)
+        if not records:
+            return
+        pdf = PdfFileWriter()
+        for record in records:
+            content, content_type = self.env.ref(
+                "mrp_workorder_data_worksheet_header."
+                "mrp_workorder_worksheet_report"
+            ).render_qweb_pdf(res_ids=record.id)
+
+            content_pdf = PdfFileReader(BytesIO(content))
+            pdf_worksheet = PdfFileReader(
+                BytesIO(b64decode(record.worksheet)),
+                strict=False)
+
+            for page in pdf_worksheet.pages:
+                new_page = pdf.addBlankPage(
+                    page.mediaBox.getWidth(), page.mediaBox.getHeight(),
+                )
+                new_page.mergePage(content_pdf.getPage(0))
+                page.scaleBy(0.95)
+                new_page.mergePage(page)
+
+        pdf_content = BytesIO()
+        pdf.write(pdf_content)
+        pdf_data = pdf_content.getvalue()
+        pdf_encoded = b64encode(pdf_data)
+        return pdf_encoded
+
+    def get_worksheets(self):
+        pdf = self.print_report()
+        if not pdf or not self.env.context.get("print", False):
+            return self.filtered("worksheet").mapped("worksheet")
+        return pdf
+
     def show_worksheets(self):
         self.ensure_one()
-        worksheets = self.mapped(
-            "finished_workorder_line_ids").get_worksheets()
+        if self.env.context.get("print_nest", False):
+            worksheets = self.mapped(
+                "finished_workorder_line_ids").get_worksheets()
+        else:
+            worksheets = self.get_worksheets()
         if not worksheets:
             return
         wizard = self.env["binary.container"].create({
