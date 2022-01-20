8d11faaf7470af01c9b1d1af1570e2313e3c1062
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-01-20 16:50:33 +0100

    [IMP] mrp_production_generate_structure: only generate for non mto

--- mrp_production_generate_structure/models/__init__.py
+++ mrp_production_generate_structure/models/__init__.py
@@ -1,5 +1,6 @@
 # Copyright 2018 Alfredo de la Fuente - Eider Oyarbide - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 from . import mrp_production
+from . import mrp_production_product_line
 from . import purchase_order
 from . import stock_rule
