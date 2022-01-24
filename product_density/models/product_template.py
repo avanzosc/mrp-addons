77315ec5915768e2550d98cdf1868253eab252e8
oihane <oihanecruce@gmail.com>
2022-01-24 09:52:27 +0100

    [ADD] New modules for density: product_density and mrp_product_density (#140)

---
+++ product_density/models/product_template.py
@@ -0,0 +1,10 @@
+# Copyright 2022 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class ProductTemplate(models.Model):
+    _inherit = "product.template"
+
+    density = fields.Float(string="Density")
