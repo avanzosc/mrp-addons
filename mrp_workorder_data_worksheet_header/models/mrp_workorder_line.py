16405ebafe2f74796838288be5af0b7fd25158db
oihane <oihanecruce@gmail.com>
2022-02-10 10:28:58 +0100

    [IMP] mrp_workorder_data_worksheet_header: fixed size to A4 + merge function (#145)

--- mrp_workorder_data_worksheet_header/models/mrp_workorder_line.py
+++ mrp_workorder_data_worksheet_header/models/mrp_workorder_line.py
@@ -1,21 +1,8 @@
 # Copyright 2021 Mikel Arregi Etxaniz - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from .mrp_workorder import print_report
 from odoo import _, models
-from base64 import b64decode, b64encode
-from io import BytesIO
-from logging import getLogger
-
-logger = getLogger(__name__)
-
-try:
-    # we need this to be sure PIL has loaded PDF support
-    from PIL import PdfImagePlugin  # noqa: F401
-except ImportError:
-    pass
-try:
-    from PyPDF2 import PdfFileWriter, PdfFileReader  # pylint: disable=W0404
-except ImportError:
-    logger.debug("Can not import PyPDF2")
 
 
 class MrpWorkorderLine(models.Model):
@@ -23,33 +10,9 @@ class MrpWorkorderLine(models.Model):
 
     def print_report(self):
         records = self.filtered(lambda r: r.finished_workorder_id.worksheet)
-        if not records:
-            return
-        pdf = PdfFileWriter()
-        for record in records:
-            content, content_type = self.env.ref(
-                "mrp_workorder_data_worksheet_header."
-                "mrp_workorder_line_worksheet_report"
-            ).render_qweb_pdf(res_ids=record.id)
-
-            content_pdf = PdfFileReader(BytesIO(content))
-            pdf_worksheet = PdfFileReader(
-                BytesIO(b64decode(record.finished_workorder_id.worksheet)),
-                strict=False)
-
-            for page in pdf_worksheet.pages:
-                new_page = pdf.addBlankPage(
-                    page.mediaBox.getWidth(), page.mediaBox.getHeight(),
-                )
-                new_page.mergePage(content_pdf.getPage(0))
-                page.scaleBy(0.95)
-                new_page.mergePage(page)
-
-        pdf_content = BytesIO()
-        pdf.write(pdf_content)
-        pdf_data = pdf_content.getvalue()
-        pdf_encoded = b64encode(pdf_data)
-        return pdf_encoded
+        return print_report(
+            records,
+            "mrp_workorder_data_worksheet_header.mrp_workorder_line_worksheet_report")
 
     def get_worksheets(self):
         pdf = self.print_report()
