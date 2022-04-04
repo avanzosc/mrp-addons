b877e74d1b03cdca5904cd06b158361408d7b5b6
oihane <oihanecruce@gmail.com>
2022-04-04 15:31:08 +0200

    [ADD] New module <mrp_bom_import> (#159)

---
+++ mrp_bom_import/models/product.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2019 Daniel Campos <danielcampos@avanzosc.es> - Avanzosc S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ProductProduct(models.Model):
+    _inherit='product.product'
+
+    map_number = fields.Char(string='Map Number')
+
+
+class ProductCategory(models.Model):
+    _inherit='product.category'
+
+    supplier_id = fields.Many2one(
+        comodel_name='res.partner', string='Supplier')
