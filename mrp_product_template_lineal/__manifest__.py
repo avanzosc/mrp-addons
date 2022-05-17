854cc4b2424e5cd516726687b5eb7ea862ed2050
Thor <alfredodelafuente@avanzosc.es>
2022-05-17 12:05:56 +0200

    [ADD] mrp_product_template_lineal: Lineal field in production order.

---
+++ mrp_product_template_lineal/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "MRP Product Template Lineal",
+    'version': '12.0.1.0.0',
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "category": "Manufacturing",
+    "depends": [
+        "mrp",
+        "product_template_lineal"
+    ],
+    "data": [
+        "views/mrp_production_views.xml",
+    ],
+    "license": "AGPL-3",
+    'installable': True,
+}
