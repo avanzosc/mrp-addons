5e692f8924f20341e64f483f3f1bd5703c97651d
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2022-05-18 17:05:26 +0200

    [ADD] mrp_product_template_lineal: Lineal field in production order. (#163)

---
+++ mrp_product_template_lineal/models/mrp_production.py
@@ -0,0 +1,13 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models, fields
+
+
+class MrpProduction(models.Model):
+    _inherit = 'mrp.production'
+
+    lineal_id = fields.Many2one(
+        string='Lineal',
+        comodel_name='product.lineal',
+        related='product_id.lineal_id',
+        store=True)
