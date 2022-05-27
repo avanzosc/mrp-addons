370e31453893a1b3afc09b5760b156145de0d680
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-05-27 12:17:12 +0200

    [FIX] mrp_cut: misstyped

--- mrp_cut/__manifest__.py
+++ mrp_cut/__manifest__.py
@@ -2,19 +2,12 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "MRP Cut",
-    'version': '14.0.1.0.0',
+    "version": "14.0.1.0.0",
     "author": "Avanzosc",
     "website": "http://www.avanzosc.es",
     "category": "MRP",
-    "depends": [
-        "product_second_uom",
-        "mrp",
-        "product_dimension"
-    ],
-    "data": [
-        "views/mrp_bom_views.xml",
-        "views/mrp_production.xml"
-    ],
+    "depends": ["product_second_uom", "mrp", "product_dimension"],
+    "data": ["views/mrp_bom_views.xml", "views/mrp_production.xml"],
     "license": "AGPL-3",
-    'installable': True,
+    "installable": True,
 }
