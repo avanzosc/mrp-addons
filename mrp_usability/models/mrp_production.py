96d88e10cccd6b61bf8ad08f6e0bd87537c66dea
oihane <oihanecruce@gmail.com>
2022-06-06 17:44:06 +0200

    [FIX] mrp_usability: remove dependency (#169)

--- mrp_usability/models/mrp_production.py
+++ mrp_usability/models/mrp_production.py
@@ -1,26 +1,27 @@
 # Copyright 2021 Berezi - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields, _
+from odoo import _, fields, models
 
 
 class MrpProduction(models.Model):
-    _inherit = 'mrp.production'
+    _inherit = "mrp.production"
 
     def _compute_workorder_count(self):
         for production in self:
             production.workorder_count = len(production.workorder_ids)
 
     workorder_count = fields.Integer(
-        '# Work Orders', compute='_compute_workorder_count')
+        string="# Work Orders", compute="_compute_workorder_count"
+    )
 
     def action_view_workorder(self):
         context = self.env.context.copy()
-        context.update({'default_production_id': self.id})
+        context.update({"default_production_id": self.id})
         return {
-            'name': _("Work Orders"),
-            'view_mode': 'tree,form',
-            'res_model': 'mrp.workorder',
-            'domain': [('id', 'in', self.workorder_ids.ids)],
-            'type': 'ir.actions.act_window',
-            'context': context
+            "name": _("Work Orders"),
+            "view_mode": "tree,form",
+            "res_model": "mrp.workorder",
+            "domain": [("id", "in", self.workorder_ids.ids)],
+            "type": "ir.actions.act_window",
+            "context": context,
         }
