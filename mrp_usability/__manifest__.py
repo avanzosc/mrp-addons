580b8f3ff8df3c2e37a42817424789aea271230f
oihane <oihanecruce@gmail.com>
2022-06-06 17:44:06 +0200

    [FIX] mrp_usability: remove dependency (#169)

--- mrp_usability/__manifest__.py
+++ mrp_usability/__manifest__.py
@@ -2,18 +2,17 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "MRP Usability",
-    'version': '14.0.2.0.0',
+    "version": "14.0.2.0.0",
     "author": "Avanzosc",
-    "website": "https://www.avanzosc.es",
+    "website": "https://github.com/OCA/l10n-spain",
     "category": "MRP",
     "depends": [
         "mrp",
-        "mrp_workorder_permission"
     ],
     "data": [
         "views/mrp_workorder_views.xml",
         "views/mrp_production_views.xml",
     ],
     "license": "AGPL-3",
-    'installable': True,
+    "installable": True,
 }
