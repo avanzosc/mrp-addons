cde2f3b63b547b0623af0eabb51c6471dd758d84
Thor <alfredodelafuente@avanzosc.es>
2022-06-07 13:24:47 +0200

    [ADD] mrp_production_duration: Put total to "expected duration" and "duration" fields.

---
+++ mrp_production_duration/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2022 Patxi lersundi 
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "MRP Production Duration",
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "category": "Manufacturing",
+    "license": "AGPL-3",
+    "version": "14.0.1.0.0",
+    "depends": ["mrp"],
+    "data": [
+        "views/mrp_production_views.xml",
+        "views/mrp_workorder_views.xml",
+        ],
+    "installable": True,
+}
