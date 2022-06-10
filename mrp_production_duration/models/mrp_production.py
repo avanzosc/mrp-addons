d1d3d28ac193ae055ad0ec3194971742d7e625b3
Oihane Crucelaegui <oihanecruce@gmail.com>
2022-06-10 11:12:53 +0200

    [IMP] mrp_production_duration: black, isort, prettier

--- mrp_production_duration/models/mrp_production.py
+++ mrp_production_duration/models/mrp_production.py
@@ -1,26 +1,32 @@
-# Copyright 2022 Patxi lersundi 
+# Copyright 2022 Patxi lersundi
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 from odoo import api, fields, models
 
 
 class MrpProduction(models.Model):
-    _inherit= 'mrp.production'
+    _inherit = "mrp.production"
 
-    total_duration_estimated = fields.Float(
-        string="Total duration estimated", store=True, copy=False,
-        compute="_compute_total_duration_estimated")
-    total_duration = fields.Float(
-        string="Total duration", store=True, copy=False,
-        compute="_compute_total_duration")
+    duration_estimated = fields.Float(
+        string="Expected Duration",
+        compute="_compute_duration_estimated",
+        copy=False,
+        store=True,
+    )
+    duration = fields.Float(
+        string="Real Duration",
+        compute="_compute_duration",
+        copy=False,
+        store=True,
+    )
 
     @api.depends("workorder_ids", "workorder_ids.duration_expected")
-    def _compute_total_duration_estimated(self):
+    def _compute_duration_estimated(self):
         for production in self:
-            production.total_duration_estimated = (
-                sum(production.workorder_ids.mapped('duration_expected')))
+            production.duration_estimated = sum(
+                production.mapped("workorder_ids.duration_expected")
+            )
 
     @api.depends("workorder_ids", "workorder_ids.duration")
-    def _compute_total_duration(self):
+    def _compute_duration(self):
         for production in self:
-            production.total_duration = (
-                sum(production.workorder_ids.mapped('duration')))
+            production.duration = sum(production.mapped("workorder_ids.duration"))
