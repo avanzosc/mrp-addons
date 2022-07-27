8131ca22a863c9bc15c37f1ca6b438d989dfe484
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2022-07-27 14:12:06 +0200

    [ADD] mrp_bom_report_group: New group "See costs in BoM" for see costs when printing "BoM structure". (#183)

---
+++ mrp_bom_report_group/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "MRP BoM Report Group",
+    'version': '14.0.1.0.0',
+    "category": "Manufacturing",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "depends": [
+        "mrp",
+    ],
+    "data": [
+        "data/mrp_bom_report_group.xml",
+        "report/mrp_bom_report.xml",
+        "views/mrp_bom_views.xml"
+    ],
+    'installable': True,
+}
