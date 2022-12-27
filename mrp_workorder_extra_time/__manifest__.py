bb1ad927ff47a676af5b49c2b81378a4c06ecba9
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2022-12-27 11:48:59 +0100

    [ADD] mrp_workorder_extra_time: In MRP workorders new fields: "Extra time piece", "Total overtime", and "Total time". (#202)

---
+++ mrp_workorder_extra_time/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Mrp Workorder Extra Time",
+    'version': '14.0.1.0.0',
+    "author": "Avanzosc",
+    "category": "Manufacturing/Manufacturing",
+    "website": "https://github.com/alfredoavanzosc/mrp-addons",
+    "license": "AGPL-3",
+    "depends": [
+        "mrp",
+    ],
+    "data": [
+        "views/mrp_workorder_views.xml",
+    ],
+    'installable': True,
+}
