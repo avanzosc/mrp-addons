99fb8074eee53fd4d745306e11211d008af2908a
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2023-02-28 10:06:14 +0100

    [ADD] mrp_set_clear_quantities: In Mrp production new buttons: "Set quantities" and "Clear quantities". (#206)

---
+++ mrp_set_clear_quantities/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "MRP Set Clear Quantities",
+    "version": "16.0.1.0.0",
+    "category": "Manufacturing/Manufacturing",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "depends": [
+        "mrp",
+        "stock"
+    ],
+    "data": [
+        "views/mrp_production_views.xml",
+    ],
+    "installable": True,
+}
