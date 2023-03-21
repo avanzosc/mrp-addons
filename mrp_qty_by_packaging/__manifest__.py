48017b892fb47b6f784cff1f5c163ad5cb0c4d1d
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2023-03-21 16:56:10 +0100

    [ADD] mrp_qty_by_packaging: In Mrp Production new fields: "Packaging", and "Packaging Quantity". (#210)

---
+++ mrp_qty_by_packaging/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Mrp Qty By Packaging",
+    "version": "16.0.1.0.0",
+    "category": "Manufacturing/Manufacturing",
+    "license": "AGPL-3",
+    "author": "https://github.com/avanzosc/sale-addons",
+    "website": "http://www.avanzosc.es",
+    "depends": [
+        "product",
+        "sale",
+        "mrp"
+    ],
+    "data": [
+        "views/mrp_production_views.xml",
+    ],
+    "installable": True,
+}
---
+++ mrp_qty_by_packaging/models/stock_rule.py
@@ -0,0 +1,25 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import models
+
+
+class StockRule(models.Model):
+    _inherit = "stock.rule"
+
+    def _prepare_mo_vals(self, product_id, product_qty, product_uom,
+                         location_dest_id, name, origin, company_id, values,
+                         bom):
+        mo_values = super(StockRule, self)._prepare_mo_vals(
+            product_id, product_qty, product_uom, location_dest_id, name,
+            origin, company_id, values, bom)
+        if ("move_dest_ids" in values and
+            len(values.get("move_dest_ids", [])) == 1 and
+                values.get("move_dest_ids")[0].sale_line_id):
+            move = values.get("move_dest_ids")[0]
+            if move.sale_line_id.product_packaging_id:
+                mo_values["product_packaging_id"] = (
+                    move.sale_line_id.product_packaging_id.id)
+            if move.sale_line_id.product_packaging_qty:
+                mo_values["product_packaging_qty"] = (
+                    move.sale_line_id.product_packaging_qty)
+        return mo_values
