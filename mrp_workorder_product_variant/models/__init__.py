d8aba48b6bf4a3c51dafe02518a3222680a3048e
Tu Nombre <you@example.com>
2023-07-13 16:03:39 +0200

    [14.0][ADD] mrp_workorder_product_variant:

---
+++ mrp_workorder_product_variant/models/__init__.py
@@ -0,0 +1,3 @@
+from . import mrp_routing_workcenter
+from . import mrp_bom
+from . import mrp_production
---
+++ mrp_workorder_product_variant/models/mrp_production.py
@@ -0,0 +1,28 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import api, models
+
+
+class MrpProduction(models.Model):
+    _inherit = "mrp.production"
+
+    @api.onchange('bom_id', 'product_id')
+    def _onchange_workorder_ids(self):
+        result = super(MrpProduction, self)._onchange_workorder_ids()
+        if self.bom_id and self.workorder_ids:
+            for workorder in self.workorder_ids:
+                attributes = self.product_id.product_template_attribute_value_ids
+                if workorder.operation_id and (
+                    workorder.operation_id.bom_product_template_attribute_value_ids) and (
+                        not any([line in attributes for line in workorder.operation_id.bom_product_template_attribute_value_ids])):
+                    self.workorder_ids = [(3, workorder.id)]
+            for line in self.move_raw_ids:
+                routing = []
+                for workorder in self.workorder_ids:
+                    if workorder.operation_id not in routing:
+                        routing.append(workorder.operation_id)
+                if line.bom_line_id and (
+                    line.bom_line_id.operation_id) and (
+                        line.bom_line_id.operation_id not in routing):
+                    self.move_raw_ids = [(3, line.id)]
+        return result
