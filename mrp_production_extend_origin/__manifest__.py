35d6e8291df204a26730477b8582061fec71a263
Alfredo <alfredodelafuente@avanzosc.es>
2023-08-08 12:14:56 +0200

    [IMP] mrp_production_extend_origin: Llevar nombre del cliente del PV al origen de la OF.

--- mrp_production_extend_origin/__manifest__.py
+++ mrp_production_extend_origin/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Mrp Production Extend Origin",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.1.0",
     "category": "MRP",
     "license": "AGPL-3",
     "author": "AvanzOSC",
