cf14900e89de1f8825672c657520683094152b72
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2023-08-24 11:14:16 +0200

    [IMP] mrp_production_extend_origin: Llevar nombre del cliente del PV al origen de la OF. (#226)

--- mrp_production_extend_origin/models/stock_rule.py
+++ mrp_production_extend_origin/models/stock_rule.py
@@ -12,13 +12,20 @@ class StockRule(models.Model):
             product_id, product_qty, product_uom, location_id, name, origin,
             company_id, values, bom)
         if "origin" in mo_values and mo_values.get("origin", False):
+            sale_obj = self.env["sale.order"]
             cond = [("name", "=", mo_values.get("origin"))]
-            sale = self.env["sale.order"].search(cond, limit=1)
+            sale = sale_obj.search(cond, limit=1)
             if sale:
                 origin = sale.name
                 if sale.origin:
                     origin = "{} // {}".format(origin, sale.origin)
                 if sale.client_order_ref:
                     origin = "{} // {}".format(origin, sale.client_order_ref)
+                if sale.origin:
+                    cond = [("name", "=", sale.origin)]
+                    sale_origin = sale_obj.sudo().search(cond, limit=1)
+                    if sale_origin:
+                        origin = "{} - {}".format(
+                            sale_origin.partner_id.name, origin)
                 mo_values["origin"] = origin
         return mo_values
