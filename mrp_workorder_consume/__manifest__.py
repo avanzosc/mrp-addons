18a0baf3d84af6933709925c467e10aaa1adff2c
Oihane Crucelaegui <oihanecruce@gmail.com>
2023-08-29 12:46:35 +0200

    [MIG] mrp_workorder_consume: Migration to 14.0

--- mrp_workorder_consume/__manifest__.py
+++ mrp_workorder_consume/__manifest__.py
@@ -3,15 +3,14 @@
 
 {
     "name": "MRP Work Order Consume",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Manufacturing/Manufacturing",
     "license": "AGPL-3",
     "author": "AvanzOSC",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/mrp-addons",
     "depends": [
         "mrp",
     ],
-    "excludes": [],
     "data": [
         "views/mrp_workorder_views.xml",
     ],
