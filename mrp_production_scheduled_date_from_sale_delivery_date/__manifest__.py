047a570de34f8137c05978777de21b73cec1e70f
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2023-08-30 08:35:34 +0200

    [ADD] mrp_production_scheduled_date_from_sale_delivery_date: When modifying the delivery date in the sale order, the commitment date of the manufacturing is also modified. (#227)

---
+++ mrp_production_scheduled_date_from_sale_delivery_date/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "MRP Production Scheduled Date From Sale Delivery Date",
+    "version": "16.0.1.0.0",
+    "category": "Manufacturing/Manufacturing",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "depends": [
+        "mrp_sale_info",
+    ],
+    "data": [
+    ],
+    "installable": True,
+}
