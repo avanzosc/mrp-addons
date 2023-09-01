08d107960d62e202a974ff1365eea31b86d1138c
oihane <oihanecruce@gmail.com>
2023-09-01 09:37:29 +0200

    [FIX] mrp_production_cost: use sudo, to avoid permission errors (#230)

--- mrp_production_cost/models/stock_move.py
+++ mrp_production_cost/models/stock_move.py
@@ -1,20 +1,24 @@
 # Copyright 2022 Berezi Amubieta - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from openerp import models, fields, api
+from openerp import api, fields, models
 
 
 class StockMove(models.Model):
-    _inherit = 'stock.move'
+    _inherit = "stock.move"
 
     standard_price = fields.Float(
-        string='Standard cost')
+        string="Standard cost",
+    )
     estimate_subcost = fields.Float(
-        string='Estimate Sub Cost',
-        compute='_compute_estimate_subcost')
+        string="Estimate Sub Cost",
+        compute="_compute_estimate_subcost",
+    )
     real_subcost = fields.Float(
-        string='Real Sub Cost',
-        compute='_compute_real_subcost',
-        store=True)
+        string="Real Sub Cost",
+        compute="_compute_real_subcost",
+        compute_sudo=True,
+        store=True,
+    )
 
     @api.depends("standard_price", "product_uom_qty")
     def _compute_estimate_subcost(self):
