aa4be4dd57c59a244f295b6bf9e34fd2cd5f343d
Oihane Crucelaegui <oihanecruce@gmail.com>
2023-09-04 13:58:38 +0200

    [MIG] mrp_stock_usability: Migration to 16.0

--- mrp_stock_usability/__manifest__.py
+++ mrp_stock_usability/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Manufacturing - Stock Usability",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Manufacturing/Manufacturing",
     "license": "AGPL-3",
     "author": "AvanzOSC",
