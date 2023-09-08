ea3d4a8e9eba58eb52a169c78864f428b64cdaab
Oihane Crucelaegui <oihanecruce@gmail.com>
2023-09-08 10:14:30 +0200

    [IMP] mrp_packaging_palet: pre-commit stuff

--- mrp_packaging_palet/models/mrp_production.py
+++ mrp_packaging_palet/models/mrp_production.py
@@ -1,21 +1,24 @@
 # Copyright 2023 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 from odoo import api, fields, models
-from odoo.tools import float_round
 
 
 class MrpProduction(models.Model):
     _inherit = "mrp.production"
 
     palet_id = fields.Many2one(
-        string="Palet", comodel_name="stock.package.type", copy=False)
+        string="Palet", comodel_name="stock.package.type", copy=False
+    )
     palet_qty = fields.Float(
-        string="Contained Palet Quantity", default=1,
-        digits="Product Unit of Measure", copy=False)
+        string="Contained Palet Quantity",
+        default=1,
+        digits="Product Unit of Measure",
+        copy=False,
+    )
 
     @api.onchange("product_packaging_id")
     def _onchange_product_packaging_id(self):
-        result = super(MrpProduction, self)._onchange_product_packaging_id()
+        result = super()._onchange_product_packaging_id()
         if self.product_packaging_id and self.product_packaging_id.palet_id.id:
             self.palet_id = self.product_packaging_id.palet_id.id
         else:
@@ -27,19 +30,19 @@ class MrpProduction(models.Model):
 
     @api.onchange("product_packaging_qty")
     def _onchange_product_packaging_qty(self):
-        result = super(MrpProduction, self)._onchange_product_packaging_qty()
+        result = super()._onchange_product_packaging_qty()
         if self.palet_id and self.product_qty:
             self.palet_qty = self._get_palet_qty()
         return result
 
     @api.onchange("product_qty")
     def _onchange_product_qty(self):
-        result = super(MrpProduction, self)._onchange_product_qty()
+        result = super()._onchange_product_qty()
         if self.palet_id and self.product_qty:
             self.palet_qty = self._get_palet_qty()
         return result
 
     def _get_palet_qty(self):
-        return (self.product_qty /
-                (self.product_packaging_id.qty *
-                 self.product_packaging_id.palet_qty))
+        return self.product_qty / (
+            self.product_packaging_id.qty * self.product_packaging_id.palet_qty
+        )
--- mrp_packaging_palet/models/stock_rule.py
+++ mrp_packaging_palet/models/stock_rule.py
@@ -6,15 +6,34 @@ from odoo import models
 class StockRule(models.Model):
     _inherit = "stock.rule"
 
-    def _prepare_mo_vals(self, product_id, product_qty, product_uom,
-                         location_dest_id, name, origin, company_id, values,
-                         bom):
-        mo_values = super(StockRule, self)._prepare_mo_vals(
-            product_id, product_qty, product_uom, location_dest_id, name,
-            origin, company_id, values, bom)
-        if ("move_dest_ids" in values and
-            len(values.get("move_dest_ids", [])) == 1 and
-                values.get("move_dest_ids")[0].sale_line_id):
+    def _prepare_mo_vals(
+        self,
+        product_id,
+        product_qty,
+        product_uom,
+        location_dest_id,
+        name,
+        origin,
+        company_id,
+        values,
+        bom,
+    ):
+        mo_values = super()._prepare_mo_vals(
+            product_id,
+            product_qty,
+            product_uom,
+            location_dest_id,
+            name,
+            origin,
+            company_id,
+            values,
+            bom,
+        )
+        if (
+            "move_dest_ids" in values
+            and len(values.get("move_dest_ids", [])) == 1
+            and values.get("move_dest_ids")[0].sale_line_id
+        ):
             move = values.get("move_dest_ids")[0]
             if move.sale_line_id.palet_id:
                 mo_values["palet_id"] = move.sale_line_id.palet_id.id
