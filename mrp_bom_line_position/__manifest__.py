28d04506bb7c78173121b95e4e17267de856636f
Tu Nombre <you@example.com>
2023-09-14 11:42:01 +0200

    [ADD] mrp_bom_line_position: new module

---
+++ mrp_bom_line_position/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "MRP BoM Line Position",
+    "version": "14.0.1.0.0",
+    "author": "Avanzosc",
+    "category": "Manufacturing/Manufacturing",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "depends": [
+        "mrp",
+    ],
+    "data": [
+        "views/mrp_bom_view.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
