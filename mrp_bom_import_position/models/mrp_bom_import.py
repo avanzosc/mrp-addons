3f82231490e012f10cb295c8ccbdea310e2d1966
Tu Nombre <you@example.com>
2023-09-15 07:32:42 +0200

    [14.0][ADD] mrp_bom_import_position:

---
+++ mrp_bom_import_position/models/mrp_bom_import.py
@@ -0,0 +1,39 @@
+# Copyright 2023 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+from odoo.addons.mrp_bom_import.models.mrp_bom_import import convert2str
+
+
+class MrpBomImport(models.Model):
+    _inherit = "mrp.bom.import"
+
+    def _get_line_values(self, row_values):
+        values = super(MrpBomImport, self)._get_line_values(row_values)
+        if values:
+            values.update(
+                {
+                    "line_position": convert2str(
+                        row_values.get("Position", "")),
+                }
+            )
+        return values
+
+
+class MrpBomLineImport(models.Model):
+    _inherit = "mrp.bom.line.import"
+
+    line_position = fields.Char(
+        string="Line Position",
+    )
+
+    def generate_bom_line_values(self):
+        self.ensure_one()
+        values = super(MrpBomLineImport, self).generate_bom_line_values()
+        values.update(
+            {
+                "line_position": self.line_position,
+            }
+        )
+        return values
