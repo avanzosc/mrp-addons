68364b4845eaa4e5170b1379a4f41b42d108256b
oihane <oihanecruce@gmail.com>
2023-09-22 17:01:34 +0200

    [FIX] custom_mrp_import_bizerba: add python dependency (#245)

--- custom_mrp_import_bizerba/__manifest__.py
+++ custom_mrp_import_bizerba/__manifest__.py
@@ -4,15 +4,11 @@
 {
     "name": "Custom MRP Import Bizerba",
     "version": "14.0.1.0.0",
-    "category": "MRP",
+    "category": "Manufacturing/Manufacturing",
     "license": "AGPL-3",
     "author": "AvanzOSC",
-    "website": "http://www.avanzosc.es",
-    "depends": [
-        "mrp",
-        "base_import_wizard",
-        "custom_mrp_descarga"
-    ],
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "depends": ["mrp", "base_import_wizard", "custom_mrp_descarga"],
     "data": [
         "security/ir.model.access.csv",
         "data/product_qty_decimal_precision.xml",
@@ -20,6 +16,6 @@
         "views/mrp_production_view.xml",
         "views/bizerba_import_line_view.xml",
     ],
-    "external_dependencies": {"python": ["xlrd"]},
+    "external_dependencies": {"python": ["xlrd", "pymssql"]},
     "installable": True,
 }
