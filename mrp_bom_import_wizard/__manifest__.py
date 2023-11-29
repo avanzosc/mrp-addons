c16f7e50fa46e2b2bb9f9285643b6175bcc142b7
Tu Nombre <you@example.com>
2023-11-29 12:38:49 +0100

    [12.0][MIG] mrp_bom_import:

---
+++ mrp_bom_import_wizard/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright (c) 2019 Daniel Campos <danielcampos@avanzosc.es> - Avanzosc S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "MRP BoM Import Wizard",
+    "version": "12.0.1.0.0",
+    "category": "Manufacturing/Manufacturing",
+    "author": "AvanzOSC",
+    "license": "AGPL-3",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "depends": [
+        "base",
+        "mrp",
+        "product",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/mrp_bom_import_view.xml",
+        "views/mrp_bom_import_line_view.xml",
+        "views/mrp_bom_line_view.xml",
+        "views/product_view.xml",
+    ],
+    "installable": True,
+}
