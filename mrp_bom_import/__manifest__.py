f9c1aeb300498ddc45f67db631ce37e72f28f41c
Unai Beristain <unaiberistain@avanzosc.es>
2024-03-08 11:36:09 +0100

    [MIG] mrp_bom_import: Migration to 16.0

--- mrp_bom_import/__manifest__.py
+++ mrp_bom_import/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "MRP BoM Import",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Manufacturing/Manufacturing",
     "author": "AvanzOSC",
     "license": "AGPL-3",
