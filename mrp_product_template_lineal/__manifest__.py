b6f6e9415f2781b61d9980c2a1c248b6f025a5a7
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2024-03-20 14:44:05 +0100

    [IMP] mrp_product_template_lineal: Show lineal field in manufacturing order report. (#282)

--- mrp_product_template_lineal/__manifest__.py
+++ mrp_product_template_lineal/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "MRP Product Template Lineal",
-    'version': '14.0.1.0.0',
+    'version': '14.0.1.1.0',
     "author": "Avanzosc",
     "website": "https://github.com/avanzosc/mrp-addons",
     "category": "Manufacturing",
@@ -12,6 +12,7 @@
     ],
     "data": [
         "views/mrp_production_views.xml",
+        "report/mrp_production_report.xml",
     ],
     "license": "AGPL-3",
     'installable': True,
