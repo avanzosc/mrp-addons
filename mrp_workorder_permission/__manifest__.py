037224641a3e3fd64013f11963cbe4378275b722
Unai Beristain <unaiberistain@avanzosc.es>
2024-03-20 16:55:53 +0100

    [MIG] mrp_workorder_permission

---
+++ mrp_workorder_permission/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2021 Berezi - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "MRP Workorder Permission",
+    "version": "16.0.1.0.0",
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "category": "Manufacturing/Manufacturing",
+    "depends": [
+        "mrp",
+    ],
+    "data": [
+        "data/mrp_workorder_permission.xml",
+        "views/mrp_workorder_views.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
