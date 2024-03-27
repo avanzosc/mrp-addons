e8d310354a4e09d58c4b31172e865b4c216f52c3
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2024-03-27 14:48:18 +0100

    [ADD] mrp_routing_workcenter_time_cycle_manual: In "Work Center Usage" new fields: "Number of insertions" and "Insertion time". (#289)

---
+++ mrp_routing_wokcenter_time_cycle_manual/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Mrp Routing Workcenter Time Cycle Manual",
+    "version": "16.0.1.0.0",
+    "category": "Manufacturing/Manufacturing",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://www.avanzosc.es",
+    "depends": [
+        "mrp",
+    ],
+    "data": [
+        "views/mrp_routing_workcenter_views.xml",
+    ],
+    "installable": True,
+}
---
+++ mrp_routing_wokcenter_time_cycle_manual/models/mrp_routing_workcenter.py
@@ -0,0 +1,21 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import api, fields, models
+
+
+class MrpRoutingWorkcenter(models.Model):
+    _inherit = "mrp.routing.workcenter"
+
+    number_insertions = fields.Integer(
+        string="Number of insertions", default=0, copy=False,
+    )
+    insertion_time = fields.Float(
+        string="Insertion time", default=0, copy=False,
+    )
+
+    @api.onchange("number_insertions", "insertion_time")
+    def onchange_insertion_number_time(self):
+        time_cycle_manual = 0
+        if self.number_insertions and self.insertion_time:
+            time_cycle_manual = self.number_insertions * self.insertion_time
+        self.time_cycle_manual = time_cycle_manual
