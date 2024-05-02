db03b4a3d6a6ed4daf7687fc58fad76abbe3d30e
Unai Beristain <unaiberistain@avanzosc.es>
2024-05-02 11:25:56 +0200

    [MIG] mrp_workorder_permission: Migration to 16.0

--- mrp_workorder_permission/__manifest__.py
+++ mrp_workorder_permission/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "MRP Workorder Permission",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Avanzosc",
     "website": "https://github.com/avanzosc/mrp-addons",
     "category": "Manufacturing/Manufacturing",
