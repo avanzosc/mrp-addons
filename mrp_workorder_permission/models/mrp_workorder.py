687095c5f6f5dee83a0df611500915de914c90b8
Unai Beristain <unaiberistain@avanzosc.es>
2024-05-02 10:27:25 +0200

    [MIG] mrp_workorder_permission: Migration to 16.0

--- mrp_workorder_permission/models/mrp_workorder.py
+++
@@ -1,9 +0,0 @@
-# Copyright 2021 Berezi - AvanzOSC
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import fields, models
-
-
-class MrpWorkorder(models.Model):
-    _inherit = "mrp.workorder"
-
-    user_id = fields.Many2one(string="User", comodel_name="res.users")
