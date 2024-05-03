c7aa6c6f563ec8597053638738b7f68a0e10b473
Unai Beristain <unaiberistain@avanzosc.es>
2024-05-03 11:58:39 +0200

    [ADD] Add modules product_brand_multi y product_supplierinfo_multibrand

---
+++ product_supplierinfo_multibrand/__openerp__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# © Copyright 2019 Mikel Arregi Etxaniz - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Product supplierinfo multibrand",
+    "version": "8.0.1.1.0",
+    "license": "AGPL-3",
+    "depends": [
+        "product_brand_multi",
+    ],
+    "author": "OdooMRP team, "
+              "AvanzOSC, "
+              "Odoo Community Association (OCA)",
+    "website": "http://www.odoomrp.com",
+    "contributors": [
+        "Mikel Arregi <mikelarregi@avanzosc.es>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+    ],
+    "category": "",
+    "summary": "",
+    "data": [
+         "views/product_view.xml",
+         "views/supplier_brand_view.xml",
+         "security/ir.model.access.csv",
+    ],
+    "installable": True,
+}
