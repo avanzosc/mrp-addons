802f6eb40eac70f876ccbd5db7abef756668d25a
Alfredo <alfredodelafuente@avanzosc.es>
2024-05-16 13:34:15 +0200

    [ADD] product_last_mrp_cost_info In products new field: "Last Manufacturing Order Cost Unit Price".

---
+++ product_last_mrp_cost_info/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Product Last MRP Cost Info",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Manufacturing",
+    "author": "Avanzosc",
+    "website": "http://www.avanzosc.com",
+    "depends": [
+        "product_last_mrp_info",
+        "mrp_stock_move_cost",
+    ],
+    "data": [
+        "views/product_product_views.xml"
+    ],
+    "installable": True,
+    "auto_install": True,
+}
---
+++ product_last_mrp_cost_info/models/product_product.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import fields, models
+
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    last_mrp_price_unit_cost = fields.Float(
+        string="Last Manufacturing Order Cost Unit Price",
+        digits="Product Price", compute="_compute_last_mrp_info"
+    )
+
+    def _get_last_mrp_info(self, production):
+        result = super(ProductProduct, self)._get_last_mrp_info(production)
+        self.last_mrp_price_unit_cost = production.price_unit_cost
+        return result
