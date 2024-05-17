d33c22a5350477b72427886e5963a7c74cff7328
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2024-05-17 10:53:24 +0200

    [ADD] mrp_machine_relation: In Work Center new field "Machine". (#300)

---
+++ mrp_machine_relation/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# Copyright 2015 Daniel Campos - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "MRP Machine Relation",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Manufacturing",
+    "description": """
+    This modules links mrp.workcenters and machines.
+    """,
+    "author": "OdooMRP team,"
+              "Avanzosc,"
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza",
+    "contributors": ["Daniel Campos <danielcampos@avanzosc.es>"],
+    "website": "http://www.avanzosc.com",
+    "depends": [
+        "mrp",
+        "machine_manager",
+    ],
+    "data": [
+        "views/mrp_workcenter_view.xml"
+    ],
+    "installable": True,
+    "auto_install": True,
+}
