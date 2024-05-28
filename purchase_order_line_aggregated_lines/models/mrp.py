01893993208cfb333dd5939a1e0394d23083177d
oihane <oihanecruce@gmail.com>
2024-05-28 09:35:19 +0200

    [FIX] purchase_order_line_aggregated_lines: raw material location (#320)

--- purchase_order_line_aggregated_lines/models/mrp.py
+++ purchase_order_line_aggregated_lines/models/mrp.py
@@ -1,30 +1,45 @@
 # Copyright 2021 Mikel Arregi Etxaniz - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import api, models, _
-from odoo.exceptions import ValidationError
+from odoo import _, api, models
+from odoo.exceptions import UserError, ValidationError
 
 
 class MrpProductionProductLine(models.Model):
-    _inherit = 'mrp.production.product.line'
+    _inherit = "mrp.production.product.line"
 
     @api.multi
     def create_automatic_purchase_order(self, origin_manufacture_order, level):
         if not self.product_id.seller_ids:
-            message = _(u"There is no vendor associated to the product {}. "
-                        "Please define a vendor for this product.").format(
-                self.product_id.name)
+            message = _(
+                "There is no vendor associated to the product {}. "
+                "Please define a vendor for this product."
+            ).format(self.product_id.name)
             raise ValidationError(message)
-        location = (self.product_id.location_id or
-                    self.env.ref('stock.stock_location_stock'))
-        rule = self.env['procurement.group']._get_rule(
-            self.product_id, location, {})
+        location = self.production_id.location_src_id or self.env.ref(
+            "stock.stock_location_stock"
+        )
+        rule = self.env["procurement.group"]._get_rule(self.product_id, location, {})
+        if not rule:
+            raise UserError(
+                _(
+                    'No procurement rule found in location "%s" for product "%s".\n '
+                    "Check routes configuration."
+                )
+                % (location.display_name, self.product_id.display_name)
+            )
         values = self._get_po_values(rule)
         rule.with_context(
             mrp_production_product_line=self,
             origin_production_id=origin_manufacture_order.id,
             level=self.production_id.level + 1,
-            analytic_account_id=self.analytic_account_id
-        ).create_merge_purchase_line(self.product_id, self.product_qty,
-                                     self.product_uom_id, location,
-                                     self.product_id.name,
-                                     self.production_id.name, False, values)
+            analytic_account_id=self.analytic_account_id,
+        ).create_merge_purchase_line(
+            self.product_id,
+            self.product_qty,
+            self.product_uom_id,
+            location,
+            self.product_id.name,
+            self.production_id.name,
+            False,
+            values,
+        )
--- purchase_order_line_aggregated_lines/models/purchase.py
+++ purchase_order_line_aggregated_lines/models/purchase.py
@@ -6,11 +6,13 @@ from odoo import api, fields, models
 class PurchaseOrderLine(models.Model):
     _inherit = "purchase.order.line"
 
-    sale_line_ids = fields.One2many(comodel_name="sale.order.line",
-                                    inverse_name="purchase_order_line_id")
+    sale_line_ids = fields.One2many(
+        comodel_name="sale.order.line", inverse_name="purchase_order_line_id"
+    )
     mrp_production_product_line_ids = fields.One2many(
         comodel_name="mrp.production.product.line",
-        inverse_name="purchase_order_line_id")
+        inverse_name="purchase_order_line_id",
+    )
     sale_qty = fields.Float(compute="_compute_total_qty")
     mrp_qty = fields.Float(compute="_compute_total_qty")
     total_qty = fields.Float(compute="_compute_total_qty")
@@ -19,8 +21,7 @@ class PurchaseOrderLine(models.Model):
     @api.depends("sale_line_ids", "mrp_production_product_line_ids")
     def _compute_total_qty(self):
         self.sale_qty = sum(self.sale_line_ids.mapped("product_qty"))
-        self.mrp_qty = sum(
-            self.mrp_production_product_line_ids.mapped("product_qty"))
+        self.mrp_qty = sum(self.mrp_production_product_line_ids.mapped("product_qty"))
         self.total_qty = self.sale_qty + self.mrp_qty
 
     @api.onchange("total_qty", "extra_qty")
