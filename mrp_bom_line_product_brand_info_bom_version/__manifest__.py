150887b9b8659469f3006b763da803530515d9e0
Alfredo <alfredodelafuente@avanzosc.es>
2024-05-29 15:09:45 +0200

    [ADD] mrp_bom_line_product_brand_info_bom_version: In "BoM With Product Brand Info" report print "Version".

---
+++ mrp_bom_line_product_brand_info_bom_version/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Mrp BoM Line Product Brand Info BoM Version",
+    "version": "16.0.1.0.0",
+    "category": "Manufacturing/Manufacturing",
+    "website": "http://www.avanzosc.es",
+    "author": "Avanzosc",
+    "license": "AGPL-3",
+    "depends": [
+        "mrp_bom_version",
+        "mrp_bom_line_product_brand_info"
+    ],
+    "data": [
+        "report/mrp_bom_report.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
