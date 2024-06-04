f426f23ab6c3d21feb7e88daf7c844d4d4650a88
Alfredo <alfredodelafuente@avanzosc.es>
2024-06-04 12:10:53 +0200

    [ADD] mrp_production_historical_bom_version:  History will be created when a LdM has a new new version, or is activated.

---
+++ mrp_production_historical_bom_version/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Mrp Production Historical Bom Version",
+    "version": "16.0.1.0.0",
+    "category": "Manufacturing/Manufacturing",
+    "website": "http://www.avanzosc.com",
+    "author": "AvanzOSC,",
+    "license": "AGPL-3",
+    
+    "depends": [
+        "mrp_production_historical",
+        "mrp_bom_version"
+    ],
+    "data": [
+    ],
+    "installable": True,
+    "auto_install": True,
+}
---
+++ mrp_production_historical_bom_version/models/mrp_bom.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import _, fields, models
+
+
+class MrpBom(models.Model):
+    _inherit = "mrp.bom"
+
+    def _copy_bom(self):
+        new_bom = super(MrpBom, self)._copy_bom()
+        new_bom._create_version_activate_historical("version")
+        return new_bom
+
+    def button_activate(self):
+        result = super(MrpBom, self).button_activate()
+        for bom in self:
+            bom._create_version_activate_historical("activated")
+        return result
+
+    def _create_version_activate_historical(self, text):
+        production_obj = self.env["mrp.production"]
+        historical_obj = self.env["mrp.production.historical"]
+        if text == "version":
+            my_text = _(u"It has been changed to version {}.").format(
+                self.version)
+        else:
+            my_text = _(u"The BoM with version {}, has been activated.").format(
+                self.version)
+        vals = {"bom_id": self.id,
+                "historical_date": fields.Datetime.now(),
+                "user_id": self.env.user.id,
+                "type": "bommod",
+                "bom_line_changes": my_text}
+        historical_obj.create(vals)
+        vals.pop("bom_id")
+        cond = [("product_tmpl_id", "=", self.product_tmpl_id.id),
+                "|", ("active", "=", True), ("active", "=", False)]
+        if self.product_id:
+            cond.append(("product_id", "=", self.product_id.id))
+        boms = self.search(cond)
+        cond = [("bom_id", "in", boms.ids),
+                ("state", "not in", ("done", "cancel", "draft"))]
+        productions = production_obj.search(cond)
+        for production in productions:
+            vals["production_id"] = production.id
+            historical_obj.with_context(from_bom_activated=True).create(vals)
