97d510307a587cc8bc91a7b0da37f5fbbe701813
Alfredo <alfredodelafuente@avanzosc.es>
2024-06-06 13:54:29 +0200

    [ADD] mrp_production_report_sale_info: In manufacturing order report print "Sale Order".

---
+++ mrp_production_report_sale_info/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Mrp Production Report Sale Info",
+    "version": "16.0.1.0.0",
+    "category": "Manufacturing/Manufacturing",
+    "website": "http://www.avanzosc.com",
+    "author": "AvanzOSC,",
+    "license": "AGPL-3",
+    "summary": "BoM notes",
+    
+    "depends": [
+        "mrp_production_report",
+        "mrp_sale_info"
+    ],
+    "data": [
+        "report/mrp_production_report.xml"
+    ],
+    "installable": True,
+    "auto_install": True,
+}
