e8f96fda921283b4e8a3c036e502de297cba42e1
Alfredo <alfredodelafuente@avanzosc.es>
2024-06-06 13:50:43 +0200

    [ADD] mrp_production_report: In manufacturing order report print this fields: "Scheduled Date", and "BoM".

---
+++ mrp_production_report/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Mrp Production Report",
+    "version": "16.0.1.0.0",
+    "category": "Manufacturing/Manufacturing",
+    "website": "http://www.avanzosc.com",
+    "author": "AvanzOSC,",
+    "license": "AGPL-3",
+    "summary": "BoM notes",
+    
+    "depends": [
+        "mrp",
+    ],
+    "data": [
+        "report/mrp_production_report.xml"
+    ],
+    "installable": True,
+}
---
+++ mrp_production_report/models/stock_move.py
@@ -0,0 +1,15 @@
+# Copyright 2014 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    def _get_lots_mrp_production_report(self):
+        lots = ""
+        lines = self.move_line_ids.filtered(lambda x: x.lot_id)
+        for line in lines:
+            lots = line.lot_id.name if not lots else "{}, {}".format(
+                lots, line.lot_id.name)
+        return lots
