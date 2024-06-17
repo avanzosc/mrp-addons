4d0daa1d7feda1c1d5046975453263d8bb82c7cd
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-17 13:44:49 +0200

    [IMP] mrp_machine_relation: pre-commit stuff

--- mrp_machine_relation/models/mrp_workcenter.py
+++ mrp_machine_relation/models/mrp_workcenter.py
@@ -1,13 +1,13 @@
-# -*- coding: utf-8 -*-
 # Copyright 2024 Alfredo de la Fuente - AvanzOSC
 # Copyright 2015 Daniel Campos - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import models, fields
+from odoo import fields, models
 
 
 class MrpWorkcenter(models.Model):
     _inherit = "mrp.workcenter"
 
     machine_id = fields.Many2one(
-        string="Machine", comodel_name="machine"
+        string="Machine",
+        comodel_name="machine",
     )
