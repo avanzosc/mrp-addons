9de7745577c88f36bcc7fb68faaa1f5f5e88bf9b
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-17 12:46:24 +0200

    [FIX] quality_control_ref_search: adjust test to v16

--- quality_control_ref_search/model/qc_inspection.py
+++ quality_control_ref_search/model/qc_inspection.py
@@ -7,17 +7,17 @@ class QcInspection(models.Model):
     _inherit = "qc.inspection"
 
     @api.depends("object_id")
-    def _generate_ref_model_name(self):
+    def _compute_ref_model_name(self):
         model_obj = self.env["ir.model"]
         for inspection in self:
             inspection.ref_model_name = False
             if inspection.object_id:
-                cond = [("model", "=", str(inspection.object_id._model))]
+                cond = [("model", "=", str(inspection.object_id._name))]
                 model = model_obj.search(cond)
                 inspection.ref_model_name = model.name
 
     @api.depends("object_id")
-    def _generate_ref_name(self):
+    def _compute_ref_name(self):
         for inspection in self.filtered(lambda x: x.object_id):
             try:
                 inspection.ref_name = inspection.object_id.name
@@ -25,6 +25,12 @@ class QcInspection(models.Model):
                 inspection.ref_name = inspection.object_id.display_name
 
     ref_model_name = fields.Char(
-        string="Ref. Model", compute="_generate_ref_model_name", store=True
+        string="Ref. Model",
+        compute="_compute_ref_model_name",
+        store=True,
     )
-    ref_name = fields.Char(string="Ref. Name", compute="_generate_ref_name", store=True)
+    ref_name = fields.Char(
+        string="Ref. Name",
+        compute="_compute_ref_name",
+        store=True,
+    )
