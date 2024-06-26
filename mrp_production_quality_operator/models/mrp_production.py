053f1cde753e68a4cc652d9c0577f32ae49eff37
Unai Beristain <unaiberis@gmail.com>
2024-06-26 09:25:07 +0200

    [ADD] Add module mrp_production_quality_operator

--- mrp_production_quality_operator/models/mrp_production.py
+++ mrp_production_quality_operator/models/mrp_production.py
@@ -4,8 +4,14 @@ from odoo import fields, models
 class MrpProduction(models.Model):
     _inherit = "mrp.production"
 
-    qty_rejected = fields.Float(string="Rejected Quantity")
-    operator_id = fields.Many2one(comodel_name="hr.employee", string="Operator")
+    qty_rejected = fields.Float(
+        string="Rejected Quantity",
+    )
+    operator_id = fields.Many2one(
+        comodel_name="hr.employee",
+        string="Operator",
+    )
     quality_responsible_id = fields.Many2one(
-        comodel_name="hr.employee", string="Quality Responsible"
+        comodel_name="hr.employee",
+        string="Manufacturer",
     )
