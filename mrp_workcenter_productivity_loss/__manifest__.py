b7d0984ccee648b9efea2894ea9f83119cf14f4e
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-27 14:35:53 +0200

    [IMP] mrp_workcenter_productivity_loss: pre-commit stuff

--- mrp_workcenter_productivity_loss/__manifest__.py
+++ mrp_workcenter_productivity_loss/__manifest__.py
@@ -2,17 +2,17 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Mrp Workcenter Productivity Loss",
-    'version': '14.0.0.0.0',
+    "version": "14.0.0.0.0",
     "category": "Manufacturing/Manufacturing",
     "license": "AGPL-3",
     "author": "AvanzOSC",
     "website": "https://github.com/avanzosc/mrp-addons",
     "depends": [
         "mrp",
-        "crm_claim_mrp"
+        "crm_claim_mrp",
     ],
     "data": [
         "views/mrp_workorder_views.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
