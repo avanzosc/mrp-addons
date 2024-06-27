0d5d2532c046c61cea6972c936910194fbe01997
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-27 14:44:38 +0200

    [IMP] mrp_production_cost: pre-commit stuff

--- mrp_production_cost/models/mrp_production.py
+++ mrp_production_cost/models/mrp_production.py
@@ -8,25 +8,43 @@ class MrpProduction(models.Model):
 
     # Material costs
     cost_material_to_consume = fields.Float(
-        string="Cost Material to consume", copy=False, store=True,
-        compute="_compute_cost_material_to_consume")
+        string="Cost Material to consume",
+        copy=False,
+        store=True,
+        compute="_compute_cost_material_to_consume",
+    )
     cost_material_consumed = fields.Float(
-        string="Cost Material consumed",  copy=False, store=True,
-        compute="_compute_cost_material_consumed")
+        string="Cost Material consumed",
+        copy=False,
+        store=True,
+        compute="_compute_cost_material_consumed",
+    )
     # Workorder costs
     cost_workorder_estimated = fields.Float(
-        string="Cost Workorder estimated", copy=False, store=True,
-        compute="_compute_cost_workorder_estimated")
+        string="Cost Workorder estimated",
+        copy=False,
+        store=True,
+        compute="_compute_cost_workorder_estimated",
+    )
     cost_workorder_real = fields.Float(
-        string="Cost Workorder real", copy=False, store=True,
-        compute='_compute_cost_workorder_real')
+        string="Cost Workorder real",
+        copy=False,
+        store=True,
+        compute="_compute_cost_workorder_real",
+    )
     # Manufacturing costs
     cost_manufacturing_estimated = fields.Float(
-        string="Cost Manufacturing estimated", copy=False, store=True,
-        compute='_compute_cost_manufacturing_estimated')
+        string="Cost Manufacturing estimated",
+        copy=False,
+        store=True,
+        compute="_compute_cost_manufacturing_estimated",
+    )
     cost_manufacturing_real = fields.Float(
-        string="Cost Manufacturing real", copy=False, store=True,
-        compute='_compute_cost_manufacturing_real')
+        string="Cost Manufacturing real",
+        copy=False,
+        store=True,
+        compute="_compute_cost_manufacturing_real",
+    )
 
     @api.depends("move_raw_ids.material_cost_to_consume")
     def _compute_cost_material_to_consume(self):
@@ -52,19 +70,21 @@ class MrpProduction(models.Model):
     @api.depends("workorder_ids.workorder_cost_real")
     def _compute_cost_workorder_real(self):
         for production in self:
-            production.cost_workorder_real = (
-                sum(production.workorder_ids.mapped("workorder_cost_real")))
+            production.cost_workorder_real = sum(
+                production.workorder_ids.mapped("workorder_cost_real")
+            )
 
     @api.depends("cost_material_to_consume", "cost_workorder_estimated")
     def _compute_cost_manufacturing_estimated(self):
         for production in self:
             production.cost_manufacturing_estimated = (
-                production.cost_material_to_consume +
-                production.cost_workorder_estimated)
+                production.cost_material_to_consume
+                + production.cost_workorder_estimated
+            )
 
     @api.depends("cost_material_consumed", "cost_workorder_real")
     def _compute_cost_manufacturing_real(self):
         for production in self:
             production.cost_manufacturing_real = (
-                production.cost_material_consumed +
-                production.cost_workorder_real)
+                production.cost_material_consumed + production.cost_workorder_real
+            )
--- mrp_production_cost/models/mrp_workorder.py
+++ mrp_production_cost/models/mrp_workorder.py
@@ -7,23 +7,33 @@ class MrpWorkorder(models.Model):
     _inherit = "mrp.workorder"
 
     machine_hour_cost = fields.Float(
-        string="Machine hour cost", readonly=True, store=True, copy=False,
-        related="workcenter_id.costs_hour")
+        string="Machine hour cost",
+        readonly=True,
+        store=True,
+        copy=False,
+        related="workcenter_id.costs_hour",
+    )
     workorder_cost_estimated = fields.Float(
-        string="Workorder Cost estimated", copy=False, store=True,
-        compute='_compute_workorder_cost_estimated')
+        string="Workorder Cost estimated",
+        copy=False,
+        store=True,
+        compute="_compute_workorder_cost_estimated",
+    )
     workorder_cost_real = fields.Float(
-        string="Workorder Cost real", copy=False, store=True,
-        compute='_compute_workorder_cost_real')
+        string="Workorder Cost real",
+        copy=False,
+        store=True,
+        compute="_compute_workorder_cost_real",
+    )
 
-    @api.depends('duration_expected', 'machine_hour_cost')
+    @api.depends("duration_expected", "machine_hour_cost")
     def _compute_workorder_cost_estimated(self):
         for order in self:
             order.workorder_cost_estimated = (
-                order.machine_hour_cost * order.duration_expected)
+                order.machine_hour_cost * order.duration_expected
+            )
 
-    @api.depends('duration', 'machine_hour_cost')
+    @api.depends("duration", "machine_hour_cost")
     def _compute_workorder_cost_real(self):
         for order in self:
-            order.workorder_cost_real = (
-                order.machine_hour_cost * order.duration)
+            order.workorder_cost_real = order.machine_hour_cost * order.duration
