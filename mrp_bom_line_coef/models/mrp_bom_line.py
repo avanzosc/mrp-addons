680c5bc30c472d1bfe08822377d424455b322188
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-27 14:15:05 +0200

    [IMP] mrp_bom_line_coef: pre-commit stuff

--- mrp_bom_line_coef/models/mrp_bom_line.py
+++ mrp_bom_line_coef/models/mrp_bom_line.py
@@ -1,22 +1,28 @@
 # Copyright 2022 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class MrpBomLine(models.Model):
-    _inherit = 'mrp.bom.line'
+    _inherit = "mrp.bom.line"
 
     coefficient = fields.Float(
-        string='Coefficient',
-        digits="Coef Decimal Precision")
-    expense_kg = fields.Boolean(string='Production Cost', default=False)
+        string="Coefficient",
+        digits="Coef Decimal Precision",
+    )
+    expense_kg = fields.Boolean(
+        string="Production Cost",
+        default=False,
+    )
     cost = fields.Float(
-        string='Fixed Cost',
-        digits="Coef Decimal Precision")
+        string="Fixed Cost",
+        digits="Coef Decimal Precision",
+    )
     currency_id = fields.Many2one(
-        string='Currency',
-        comodel_name='res.currency',
-        default=lambda self: self.env.company.currency_id.id)
+        string="Currency",
+        comodel_name="res.currency",
+        default=lambda self: self.env.company.currency_id.id,
+    )
 
     @api.onchange("product_id")
     def onchange_cost(self):
