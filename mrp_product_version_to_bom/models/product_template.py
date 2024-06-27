690df5965d3968bd98bc27626522350be38cb41b
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-27 14:39:39 +0200

    [IMP] mrp_product_version_to_bom: pre-commit stuff

--- mrp_product_version_to_bom/models/product_template.py
+++ mrp_product_version_to_bom/models/product_template.py
@@ -1,19 +1,23 @@
 # Copyright 2021 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     last_version = fields.Char(
-        string='Last Version', compute='_compute_last_version', store=True)
+        string="Last Version",
+        compute="_compute_last_version",
+        store=True,
+    )
 
-    @api.depends('bom_line_ids', 'bom_line_ids.version')
+    @api.depends("bom_line_ids", "bom_line_ids.version")
     def _compute_last_version(self):
         for product in self:
             if product.bom_line_ids:
                 product.last_version = max(
-                    product.bom_line_ids, key=lambda x: int(x.version)).version
+                    product.bom_line_ids, key=lambda x: int(x.version)
+                ).version
             else:
-                product.last_version = '00'
+                product.last_version = "00"
