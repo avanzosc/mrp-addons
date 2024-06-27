35836079c88ea5b4c2331f32220a11444c2e33e6
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-27 14:44:38 +0200

    [IMP] mrp_production_cost: pre-commit stuff

--- mrp_production_cost/models/stock_move.py
+++ mrp_production_cost/models/stock_move.py
@@ -1,29 +1,41 @@
 # Copyright 2022 Patxi Lersundi
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-from odoo import api, models, fields
+from odoo import api, fields, models
 
 
 class StockMove(models.Model):
     _inherit = "stock.move"
 
     product_standard_cost = fields.Float(
-        string="Standard Cost", readonly=True, store=True, copy=False,
-        related='product_id.standard_price')
+        string="Standard Cost",
+        readonly=True,
+        store=True,
+        copy=False,
+        related="product_id.standard_price",
+    )
     material_cost_to_consume = fields.Float(
-        string="Material cost to consume", store=True, copy=False,
-        compute='_compute_material_cost_to_consume')
+        string="Material cost to consume",
+        store=True,
+        copy=False,
+        compute="_compute_material_cost_to_consume",
+    )
     material_cost_consumed = fields.Float(
-        string="Material cost consumed", store=True, copy=False,
-        compute='_compute_material_cost_consumed')
+        string="Material cost consumed",
+        store=True,
+        copy=False,
+        compute="_compute_material_cost_consumed",
+    )
 
-    @api.depends('product_standard_cost', 'product_uom_qty')
+    @api.depends("product_standard_cost", "product_uom_qty")
     def _compute_material_cost_to_consume(self):
         for move in self:
             move.material_cost_to_consume = (
-                move.product_standard_cost * move.product_uom_qty)
+                move.product_standard_cost * move.product_uom_qty
+            )
 
-    @api.depends('product_standard_cost', 'quantity_done')
+    @api.depends("product_standard_cost", "quantity_done")
     def _compute_material_cost_consumed(self):
         for move in self:
             move.material_cost_consumed = (
-                move.product_standard_cost * move.quantity_done)
+                move.product_standard_cost * move.quantity_done
+            )
