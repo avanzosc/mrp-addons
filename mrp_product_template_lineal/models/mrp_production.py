4d18a50c5091bc4367741185ddf5d0cc3ec8f094
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-06-27 14:38:19 +0200

    [IMP] mrp_product_template_lineal: pre-commit stuff

--- mrp_product_template_lineal/models/mrp_production.py
+++ mrp_product_template_lineal/models/mrp_production.py
@@ -1,13 +1,14 @@
 # Copyright 2022 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from odoo import models, fields
+from odoo import fields, models
 
 
 class MrpProduction(models.Model):
-    _inherit = 'mrp.production'
+    _inherit = "mrp.production"
 
     lineal_id = fields.Many2one(
-        string='Lineal',
-        comodel_name='product.lineal',
-        related='product_id.lineal_id',
-        store=True)
+        string="Lineal",
+        comodel_name="product.lineal",
+        related="product_id.lineal_id",
+        store=True,
+    )
