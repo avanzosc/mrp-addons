9e1287702653772c465028b8594dfb353612d492
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2024-07-11 09:48:39 +0200

    [ADD] mrp_bom_multiple_production_qty: Adding field to set production… (#374)
    * [ADD] mrp_bom_multiple_production_qty: Adding field to set production multiple in the LdM for productions.
    
    * Add generated README and setup files

---
+++ mrp_bom_multiple_production_qty/__manifest__.py
@@ -0,0 +1,15 @@
+# (c) José A. Ballate - Guadaltech
+# (c) Ignacio Ales López - Guadaltech
+# (c) 2024 Alfredo de la Fuente - Avanzosc
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "MRP Bom Multiple Production Qty",
+    "version": "16.0.1.0.0",
+    "category": "Manufacturing",
+    "author": "Guadaltech Soluciones Tecnológicas S.L., AvanzOSC",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "license": "LGPL-3",
+    "depends": ["mrp"],
+    "data": ["views/mrp_bom.xml"],
+    "installable": True,
+}
