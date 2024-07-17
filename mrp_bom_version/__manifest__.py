a9367c660ab38515a822831b156dac6319e0fc99
Alfredo <alfredodelafuente@avanzosc.es>
2024-07-17 14:05:15 +0200

    [ADD] mrp_bom_version: BoM with version.

---
+++ mrp_bom_version/__manifest__.py
@@ -0,0 +1,32 @@
+# (c) 2015 Alfredo de la Fuente - AvanzOSC
+# (c) 2015 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "MRP - BoM version",
+    "version": "16.0.1.0.0",
+    "category": "Manufacturing/Manufacturing",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "author": "OdooMRP team,"
+    "AvanzOSC,"
+    "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
+    "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "summary": "BoM versioning",
+    "contributors": [
+        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
+        "Ana Juaristi <anajuaristi@avanzosc.es>",
+        "Alfredo de la Fuente <alfredodelafuente@avanzosc.es>",
+        "Oihane Crucelaegui <oihanecrucelaegui@avanzosc.es>",
+    ],
+    "depends": ["mail", "mrp", "mrp_bom_report"],
+    "data": [
+        "data/mrp_bom_data.xml",
+        "security/mrp_bom_version_security.xml",
+        "report/mrp_bom_report.xml",
+        "views/res_config_settings_views.xml",
+        "views/mrp_bom_view.xml",
+    ],
+    "installable": True,
+    "post_init_hook": "_post_install_set_active_bom_active_state",
+}
