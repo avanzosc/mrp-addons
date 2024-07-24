51ffc6a10fe73396bb59dde43f9e154b08ca044b
Alfredo <alfredodelafuente@avanzosc.es>
2024-07-24 11:11:00 +0200

    [14.0][ADD] mrp_bom_line_semi_finished: In product template new field "Is semi-finished?".

---
+++ mrp_bom_line_semi_finished/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Mrp BoM Line Semi Finished",
+    "version": "14.0.1.0.0",
+    "category": "MRP",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "depends": ["product", "sale", "stock", "mrp", "uom"],
+    "data": [
+        "views/product_template_views.xml",
+        "views/product_product_views.xml",
+        "views/mrp_bom_views.xml",
+        "views/mrp_bom_line_views.xml",
+        "views/sale_order_views.xml",
+    ],
+    "installable": True,
+}
---
+++ mrp_bom_line_semi_finished/models/mrp_bom_line.py
@@ -0,0 +1,34 @@
+# Copyright 2023 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import fields, models
+
+
+class MrpBomLine(models.Model):
+    _inherit = "mrp.bom.line"
+
+    semi_finished = fields.Boolean(
+        string="Is semi-finished?", related="product_id.semi_finished"
+    )
+    qty_available = fields.Float(
+        string="Quantity On Hand",
+        related="product_id.qty_available",
+        digits="Product Unit of Measure",
+    )
+    virtual_available = fields.Float(
+        string="Forecast Quantity",
+        related="product_id.virtual_available",
+        digits="Product Unit of Measure",
+    )
+    product_to_produce_id = fields.Many2one(
+        string="Product to produce",
+        comodel_name="product.product",
+        related="bom_id.product_id",
+        store=True,
+        copy=False,
+    )
+    bom_code = fields.Char(
+        string="BoM Reference", related="bom_id.code", store=True, copy=False
+    )
+    bom_type = fields.Selection(
+        string="BoM Type", related="bom_id.type", store=True, copy=False
+    )
