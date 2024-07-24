d2bd7cfe30f25c5b447fe331f311586f58f06981
Alfredo <alfredodelafuente@avanzosc.es>
2024-07-24 08:15:35 +0200

    [16.0][ADD] mrp_production_historical_follower: In user new field "Is a production follower".

---
+++ mrp_production_historical_follower/models/mrp_production.py
@@ -0,0 +1,23 @@
+# Copyright 2014 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+from odoo import api, fields, models
+
+
+class MrpProduction(models.Model):
+    _inherit = "mrp.production"
+
+    mrp_production_follower_ids = fields.Many2many(
+        comodel_name="res.partner", string="Production followers"
+    )
+
+    @api.model_create_multi
+    def create(self, values):
+        productions = super().create(values)
+        cond = [("mrp_production_follower", "=", True)]
+        users = self.env["res.users"].search(cond)
+        if users:
+            for production in productions:
+                production.mrp_production_follower_ids = [
+                    (6, 0, users.mapped("partner_id").ids)
+                ]
+        return productions
