d4ad6194af28b6bbec5cacd65ba8c4019ee5bd5f
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2024-08-06 11:11:46 +0200

    [16.0][ADD] mrp_bom_version_note: "Notes" in BoM. (#398)
    Co-authored-by: Ana Juaristi <ajuaristio@gmail.com>

---
+++ mrp_bom_version_note/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "Mrp Bom Version Note",
+    "version": "16.0.1.0.0",
+    "category": "Manufacturing/Manufacturing",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "author": "AvanzOSC,",
+    "license": "AGPL-3",
+    "summary": "BoM notes",
+    "depends": [
+        "mrp_bom_version",
+        "mrp_bom_note",
+    ],
+    "data": ["views/mrp_bom_views.xml"],
+    "installable": True,
+    "auto_install": True,
+}
