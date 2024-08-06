d8575a607c52ead9de66376520bca6f5129cf20e
alfredoavanzosc <alfredodelafuente@avanzosc.es>
2024-08-06 12:12:39 +0200

    [ADD] mrp_bom_report: In "Bill of Material" object new report "BoM Customized Report". (#383)

---
+++ mrp_bom_report/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Mrp BoM Report",
+    "version": "16.0.1.0.0",
+    "category": "Manufacturing/Manufacturing",
+    "author": "Avanzosc",
+    "license": "AGPL-3",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "depends": [
+        "mrp",
+    ],
+    "data": [
+        "data/paperformat.xml",
+        "report/external_layout.xml",
+        "report/mrp_bom_report.xml",
+    ],
+    "installable": True,
+}
