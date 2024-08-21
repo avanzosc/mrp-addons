52c93fadeac8059824db4574a502135d45de6d02
Alfredo <alfredodelafuente@avanzosc.es>
2024-08-21 13:28:59 +0200

    [16.0][MIG] mrp_production_initial_quality_control: Migration to 16.0

--- mrp_production_initial_quality_control/__manifest__.py
+++ mrp_production_initial_quality_control/__manifest__.py
@@ -1,24 +1,22 @@
-# -*- coding: utf-8 -*-
 # Copyright 2019 Alfredo de la Fuente - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
 {
     "name": "MRP Production Initial Quality Control",
     "summary": "Customization Module",
-    "version": "8.0.1.0.0",
-    "category": "Manufacturing",
+    "version": "16.0.1.0.0",
+    "category": "Quality control",
     "license": "AGPL-3",
     "author": "AvanzOSC",
-    "website": "http://www.avanzosc.es",
+    "website": "https://github.com/avanzosc/mrp-addons",
     "contributors": [
         "Ana Juaristi <anajuaristi@avanzosc.es>",
         "Alfredo de la Fuente <alfredodelafuente@avanzosc.es>",
     ],
     "depends": [
-        "quality_control_mrp",
+        "quality_control_mrp_oca",
     ],
     "data": [
-        'data/mrp_production_initial_quality_control_data.xml',
+        "data/mrp_production_initial_quality_control_data.xml",
     ],
     "installable": True,
 }
