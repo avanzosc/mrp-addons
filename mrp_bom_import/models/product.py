14ec7f09994340331dcd602d96b3161396c662cc
Unai Beristain <unaiberis@gmail.com>
2024-09-12 15:22:54 +0200

    [MIG] mrp_bom_import: Migration to 16.0

--- mrp_bom_import/models/product.py
+++ mrp_bom_import/models/product.py
@@ -7,10 +7,4 @@ from odoo import fields, models
 class ProductProduct(models.Model):
     _inherit = "product.product"
 
-    map_number = fields.Char(string="Map Number")
-
-
-class ProductCategory(models.Model):
-    _inherit = "product.category"
-
-    supplier_id = fields.Many2one(comodel_name="res.partner", string="Supplier")
+    map_number = fields.Char()
