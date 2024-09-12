0a28486a29e07bf7608e6ef9be72f0d94f4924ff
Oihane Crucelaegui <oihanecruce@gmail.com>
2024-09-12 15:25:50 +0200

    [FIX] mrp_production_deconstruction: Fix precomputed field warning

--- mrp_production_deconstruction/__manifest__.py
+++ mrp_production_deconstruction/__manifest__.py
@@ -1,11 +1,11 @@
 # Copyright 2022 Berezi Amubieta - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
-    "name": "MRP Production Deconstruction",
+    "name": "Production Deconstruction",
     "version": "16.0.1.0.0",
     "author": "Avanzosc",
     "website": "https://github.com/avanzosc/mrp-addons",
-    "category": "MRP",
+    "category": "Manufacturing/Manufacturing",
     "depends": [
         "mrp",
     ],
