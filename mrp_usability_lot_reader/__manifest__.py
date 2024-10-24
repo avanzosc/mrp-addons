4c7d37d35b476b0bb3ce99557de2ffe7b30dd7a5
Alfredo <alfredodelafuente@avanzosc.es>
2024-10-24 13:33:54 +0200

    [16.0][ADD] mrp_usability_lot_reader: In detailed operations of manufacturing orders new field "Reader".

---
+++ mrp_usability_lot_reader/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Mrp Usability Lot Reader",
+    "version": "16.0.1.0.0",
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "category": "Manufacturing/Manufacturing",
+    "license": "AGPL-3",
+    "depends": ["stock_move_line_product_lot_reader", "mrp_usability"],
+    "data": ["views/mrp_production_views.xml"],
+    "installable": True,
+    "auto_install": True,
+}
