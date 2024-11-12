c5c1e15857c4e8bb335a64ee738be00529c708fe
Alfredo <alfredodelafuente@avanzosc.es>
2024-11-12 08:01:22 +0100

    [16.0][ADD] mrp_workorder_qr_report: In Work Order new report "QR Work Order".

---
+++ mrp_workorder_qr_report/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Mrp Workorder QR Report",
+    "version": "16.0.1.0.0",
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "category": "Manufacturing/Manufacturing",
+    "license": "AGPL-3",
+    "depends": ["mrp"],
+    "data": [
+        "report/mrp_workorder_report.xml",
+    ],
+    "installable": True,
+}
