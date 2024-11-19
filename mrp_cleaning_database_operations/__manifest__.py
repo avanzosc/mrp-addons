2a8dd553dca4d380641ecb31fdaf11dbe7b9881b
Berezi <berezi@avanzosc.home>
2024-11-19 15:50:47 +0100

    [16.0][IMP] mrp_cleaning_database_operations: Cleaning all the MRP operations: MRP Production and MRP Workorder.

---
+++ mrp_cleaning_database_operations/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "MRP Cleaning Database Operations",
+    "version": "16.0.1.0.0",
+    "category": "Manufacture",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "depends": [
+        "cleaning_database_operations",
+        "mrp",
+    ],
+    "data": [
+        "views/cleaning_database_view.xml",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
