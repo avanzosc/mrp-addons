54661f4634d5fd8b892d90ac4c8007e50d0fd7f3
Unai Beristain <unaiberis@gmail.com>
2025-01-10 12:46:25 +0100

    [IMP] mrp_bom_import_line_layer_and_designator: Add update button

--- mrp_bom_import_line_layer_and_designator/models/mrp_bom_import.py
+++ mrp_bom_import_line_layer_and_designator/models/mrp_bom_import.py
@@ -1,4 +1,4 @@
-from odoo import models
+from odoo import _, models
 
 from odoo.addons.mrp_bom_import.models.mrp_bom_import import convert2str
 
@@ -15,3 +15,59 @@ class MrpBomImport(models.Model):
             }
         )
         return res
+
+    def action_update_layer_and_designator(self):
+        BomLine = self.env["mrp.bom.line"]
+
+        for record in self:
+            for line in record.bom_line_import_ids:
+
+                layer, designator = convert2str(line.layer), convert2str(
+                    line.designator
+                )
+                search_domain = [
+                    ("product_id", "=", line.product_id.id),
+                    (
+                        "bom_id.product_tmpl_id",
+                        "=",
+                        line.bom_product_id.product_tmpl_id.id,
+                    ),
+                    "|",
+                    ("bom_id.product_id", "=", line.bom_product_id.id),
+                    ("bom_id.product_id", "=", False),
+                ]
+
+                bom_lines = BomLine.search(search_domain)
+
+                if bom_lines:
+                    for bom_line in bom_lines:
+                        line.log_info = _("Found BOM Line")
+
+                        if layer and bom_line.layer in [False, None, ""]:
+                            bom_line.layer = layer
+                            line.log_info = _("Layer updated to '%s'") % layer
+                            line.state = "done"
+                        else:
+                            if not layer:
+                                line.log_info = _("No layer provided")
+                            elif bom_line.layer not in [False, None, ""]:
+                                line.log_info = (
+                                    _("Layer already set to '%s'") % bom_line.layer
+                                )
+
+                        if designator and bom_line.designator in [False, None, ""]:
+                            bom_line.designator = designator
+                            line.log_info = _("Designator updated to '%s'") % designator
+                            line.state = "done"
+                        else:
+                            if not designator:
+                                line.log_info = _("No designator provided")
+                            elif bom_line.designator not in [False, None, ""]:
+                                line.log_info = (
+                                    _("Designator already set to '%s'")
+                                    % bom_line.designator
+                                )
+
+                else:
+                    line.log_info = _("No BOM lines found\n%s") % str(search_domain)
+                    line.state = "error"
