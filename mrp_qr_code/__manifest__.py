c14315615d07025aadc6788887053c70076a0d0f
Unai Beristain <unaiberis@gmail.com>
2025-01-24 10:28:08 +0100

    [ADD] mrp_qr_code: Create field qr_code in mrp models

---
+++ mrp_qr_code/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "Product QR Code",
+    "version": "16.0.1.0.0",
+    "category": "Product",
+    "author": "Avanzosc",
+    "license": "LGPL-3",
+    "depends": [],
+    "data": [],
+    "installable": True,
+    "application": False,
+    "website": "https://github.com/avanzosc/mrp-addons",
+}
---
+++ mrp_qr_code/models/mrp_production.py
@@ -0,0 +1,32 @@
+import base64
+from io import BytesIO
+
+import qrcode
+
+from odoo import models
+
+
+class MrpProduction(models.Model):
+    _inherit = "mrp.production"
+
+    def _create_qr_code(self, integer_to_convert):
+        if integer_to_convert:
+            sequence_str = str(integer_to_convert)
+
+            qr = qrcode.QRCode(
+                version=1,
+                error_correction=qrcode.constants.ERROR_CORRECT_L,
+                box_size=10,
+                border=4,
+            )
+            qr.add_data(sequence_str)
+            qr.make(fit=True)
+
+            img = qr.make_image(fill_color="black", back_color="white")
+            buffer = BytesIO()
+            img.save(buffer, format="PNG")
+            qr_image = base64.b64encode(buffer.getvalue()).decode("utf-8")
+
+            return qr_image
+        else:
+            return False
