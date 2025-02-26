060be02e7d4a84e928c644a49addb81135e36512
Berezi <berezi@avanzosc.home>
2025-02-26 12:24:54 +0100

    [16.0][ADD] mrp_production_emptying_lots: New module to emptying lots in source location.

---
+++ mrp_production_emptying_lots/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Berezi Amubieta - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "MRP Production Emptying Lots",
+    "version": "16.0.1.0.0",
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "category": "Manufacturing/Manufacturing",
+    "depends": ["mrp_usability", "stock_picking_emptying_lots"],
+    "data": [
+        "views/mrp_production_view.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
