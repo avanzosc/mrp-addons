d3c755210ebdcee0d5c4d2cdc3023f8c3924e5b8
Unai Beristain <unaiberis@gmail.com>
2025-03-21 10:56:25 +0100

    [IMP] mrp_workorder_productivity_calendar_id: Add calendar_id

---
+++ mrp_workorder_productivity_calendar_id/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "MRP Workorder Productivity Calendar",
+    "version": "14.0.1.0.0",
+    "category": "MRP",
+    "author": "Avanzosc",
+    "license": "LGPL-3",
+    "depends": ["mrp"],
+    "data": ["views/mrp_workcenter_productivity_views.xml"],
+    "installable": True,
+    "application": False,
+    "website": "https://github.com/avanzosc/mrp-addons",
+}
---
+++ mrp_workorder_productivity_calendar_id/models/mrp_workcenter_productivity.py
@@ -0,0 +1,35 @@
+from odoo import api, fields, models
+
+
+class MrpWorkcenterProductivity(models.Model):
+    _inherit = "mrp.workcenter.productivity"
+
+    calendar_id = fields.Many2one(
+        "resource.calendar",
+        string="Work Schedule",
+        required=True,
+        help="Work schedule of the employee at the time of this record.",
+    )
+
+    @api.onchange("user_id")
+    def _onchange_user_id(self):
+        """When user_id changes, fetch the employee's work schedule and store it."""
+        if self.user_id:
+            employee = self.env["hr.employee"].search(
+                [("user_id", "=", self.user_id.id)], limit=1
+            )
+            if employee and employee.resource_calendar_id:
+                self.calendar_id = employee.resource_calendar_id
+        else:
+            self.calendar_id = False
+
+    @api.model
+    def create(self, vals):
+        """Ensure calendar_id is set when creating a record with a user_id."""
+        if "user_id" in vals and vals["user_id"] and "calendar_id" not in vals:
+            employee = self.env["hr.employee"].search(
+                [("user_id", "=", vals["user_id"])], limit=1
+            )
+            if employee and employee.resource_calendar_id:
+                vals["calendar_id"] = employee.resource_calendar_id.id
+        return super(MrpWorkcenterProductivity, self).create(vals)
