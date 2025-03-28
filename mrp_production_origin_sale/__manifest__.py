08189ca65b473ce49652698f07ed4a8cdf1d0d88
Unai Beristain <unaiberis@gmail.com>
2025-03-28 10:09:09 +0100

    [IMP] mrp_workorder_origin_sale: Concatenate origin field

---
+++ mrp_production_origin_sale/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    "name": "MRP Production Origin Sale",
+    "version": "16.0.1.0.0",
+    "category": "Manufacturing",
+    "author": "Avanzosc",
+    "license": "LGPL-3",
+    "depends": ["mrp"],
+    "data": [],
+    "installable": True,
+    "application": False,
+    "website": "https://github.com/avanzosc/mrp-addons",
+}
