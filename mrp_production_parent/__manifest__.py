cedaf6bd7e75b72f432df3c67b1892d5174a67ab
Alfredo <alfredodelafuente@avanzosc.es>
2025-04-04 10:03:16 +0200

    [16.0][ADD] mrp_production_parent: In production new field "Production Parent.

---
+++ mrp_production_parent/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2025 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Mrp Production Parent",
+    "version": "16.0.1.0.0",
+    "category": "Manufacturing",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "author": "AvanzOSC",
+    "license": "AGPL-3",
+    "depends": ["mrp_sale_info"],
+    "data": [
+        "views/mrp_production_views.xml",
+    ],
+    "installable": True,
+}
