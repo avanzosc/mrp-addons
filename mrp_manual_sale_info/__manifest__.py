a6c68e3337bd75f130499df98a83973ec4f3e9cf
Oihane Crucelaegui <oihanecruce@gmail.com>
2025-05-14 12:23:57 +0200

    [MIG] mrp_manual_sale_info: Migration to 16.0

--- mrp_manual_sale_info/__manifest__.py
+++ mrp_manual_sale_info/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "MRP Manual Sale Info",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Manufacturing",
     "license": "AGPL-3",
     "author": "AvanzOSC",
