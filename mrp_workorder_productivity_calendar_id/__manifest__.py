7c89a3778ffeff6c1865f265daf469f31c85616c
luciaavanzosc <luciaavanzosc@gmail.com>
2025-05-26 08:52:48 +0200

    [MIG][16.0] mrp_workorder_productivity_calendar_id: Migration to 16.0.

---
+++ mrp_workorder_productivity_calendar_id/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2025 Aner Arregi - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "MRP Workorder Productivity Calendar",
+    "version": "16.0.1.0.0",
+    "category": "Manufacturing/Manufacturing",
+    "author": "Avanzosc",
+    "license": "LGPL-3",
+    "depends": [
+        "mrp",
+        "hr",
+    ],
+    "data": ["views/mrp_workcenter_productivity_views.xml"],
+    "installable": True,
+    "website": "https://github.com/avanzosc/mrp-addons",
+}
