68de94876176004407eb844cbe234968a78da351
Oihane Crucelaegui <oihanecruce@gmail.com>
2025-06-23 12:16:58 +0200

    [IMP] mrp_bom_import: add dependence with base_import

--- mrp_bom_import/__manifest__.py
+++ mrp_bom_import/__manifest__.py
@@ -3,15 +3,14 @@
 
 {
     "name": "MRP BoM Import",
-    "version": "14.0.1.0.0",
-    "category": "Manufacturing/Manufacturing",
+    "version": "14.0.2.0.0",
+    "category": "Hidden/Tools",
     "author": "AvanzOSC",
     "license": "AGPL-3",
     "website": "https://github.com/avanzosc/mrp-addons",
     "depends": [
-        "base",
         "mrp",
-        "product",
+        "base_import_wizard",
     ],
     "data": [
         "security/ir.model.access.csv",
