89524c6462b9fa23cf8122fcdf5deaec59178825
luciaavanzosc <luciaavanzosc@gmail.com>
2025-07-16 11:55:36 +0200

    [16.0][ADD] mrp_workorder_quant_manual_assign: Adds Stock Quant Manual Assign button in WOs.

---
+++ mrp_workorder_quant_manual_assign/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2025 Lucía Echeverría - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "MRP Workorder Quant Manual Assign",
+    "summary": "Adds Stock Quant Manual Assign button in WOs.",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": ["mrp_production_quant_manual_assign"],
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "category": "Manufacturing",
+    "data": [
+        "views/mrp_workorder_views.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
