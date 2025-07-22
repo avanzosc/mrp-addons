79c12c234058d6b2d3f0b399dec4e44799532192
Alfredo <alfredodelafuente@avanzosc.es>
2025-07-22 11:15:45 +0200

    [16.0][ADD] mrp_production_scrap_quantity: In production order new fieds: "Scrap Qty" and "Real Quantity Produced".

---
+++ mrp_production_scrap_quantity/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2025 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Mrp Production Scrap Quantity",
+    "version": "16.0.1.0.0",
+    "category": "Manufacturing/Manufacturing",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "author": "AvanzOSC",
+    "license": "AGPL-3",
+    "depends": [
+        "mrp",
+    ],
+    "data": [
+        "views/mrp_production_views.xml",
+    ],
+    "installable": True,
+}
