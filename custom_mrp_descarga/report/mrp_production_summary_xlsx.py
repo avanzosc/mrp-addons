0aef8051a4d539c09ab38593b6d51c2c514495e3
Ane <aneavanzosc@gmail.com>
2025-08-04 14:02:00 +0200

    time correction

--- custom_mrp_descarga/report/mrp_production_summary_xlsx.py
+++ custom_mrp_descarga/report/mrp_production_summary_xlsx.py
@@ -339,9 +339,9 @@ class ReportMrpProductionsummaryXlsx(models.AbstractModel):
         )
         total_seconds = int(average_matanza_speed * 60)
         hours = total_seconds // 3600
-        mins = (total_seconds % 3600) // 60
+        minutes = (total_seconds % 3600) // 60
         seconds = total_seconds % 60
-        hhmm = f"{mins:02d}:{seconds:02d}"
+        hhmm = f"{minutes:02d}:{seconds:02d}"
         n += 1
         m = 0
         worksheet.set_column(0, 0, 40) 
