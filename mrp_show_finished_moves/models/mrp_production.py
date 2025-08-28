4ca2f5a2818c7a5beeec22c5bb8a3fa381a7abac
luciaavanzosc <luciaavanzosc@gmail.com>
2025-08-28 11:19:36 +0200

    [16.0][IMP] mrp_show_finished_moves: Add shortcut button to MOs, WOs and Finished Moves to see all related finished move lines' packages.

--- mrp_show_finished_moves/models/mrp_production.py
+++ mrp_show_finished_moves/models/mrp_production.py
@@ -21,6 +21,19 @@ class MrpProduction(models.Model):
             "context": {"default_procurement_group_id": self.procurement_group_id.id},
         }
 
+    def action_show_finished_move_lines_result_packages(self):
+        self.ensure_one()
+        finished_lines_packages = self.mapped(
+            "move_finished_ids.move_line_ids.result_package_id"
+        )
+        return {
+            "type": "ir.actions.act_window",
+            "name": "Result Packages",
+            "view_mode": "tree",
+            "res_model": "stock.quant.package",
+            "domain": [("id", "in", finished_lines_packages.ids)],
+        }
+
     @api.depends(
         "workorder_ids.state", "move_finished_ids", "move_finished_ids.quantity_done"
     )
--- mrp_show_finished_moves/models/stock_move.py
+++ mrp_show_finished_moves/models/stock_move.py
@@ -17,3 +17,16 @@ class StockMove(models.Model):
         for move in self:
             if move.production_id.qty_producing == 0.0:
                 move.production_id.qty_producing = move.quantity_done
+
+    def action_show_move_lines_packages(self):
+        self.ensure_one()
+        origin_packages = self.mapped("move_line_ids.package_id")
+        result_packages = self.mapped("move_line_ids.result_package_id")
+        all_packages = (origin_packages | result_packages).filtered(lambda p: p)
+        return {
+            "type": "ir.actions.act_window",
+            "name": "Packages",
+            "view_mode": "tree",
+            "res_model": "stock.quant.package",
+            "domain": [("id", "in", all_packages.ids)],
+        }
