6152be71200fc6a5a1e13248e387dbcef3731502
Oihane Crucelaegui <oihanecruce@gmail.com>
2025-08-29 10:25:03 +0200

    [FIX] mrp_workorder_components_availability: related fields do not require selection

--- mrp_workorder_components_availability/models/mrp_workorder.py
+++ mrp_workorder_components_availability/models/mrp_workorder.py
@@ -8,26 +8,17 @@ class MrpWorkorder(models.Model):
         related="production_id.components_availability",
         string="MO Component Status",
         readonly=True,
-        help=(
-            "Latest component availability status for parent MO. "
-            "If green, then the MO's readiness status is ready, "
-            "as per BOM configuration."
-        ),
+        help="Latest component availability status for parent MO. If green, "
+        "then the MO's readiness status is ready, as per BOM configuration.",
     )
 
     mo_components_availability_state = fields.Selection(
-        [("available", "Available"), ("expected", "Expected"), ("late", "Late")],
         related="production_id.components_availability_state",
         readonly=True,
         string="MO Components Availability State",
     )
 
     mo_reservation_state = fields.Selection(
-        [
-            ("confirmed", "Waiting"),
-            ("assigned", "Ready"),
-            ("waiting", "Waiting Another Operation"),
-        ],
         string="MO Readiness",
         related="production_id.reservation_state",
         readonly=True,
