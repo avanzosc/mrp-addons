5f2dc784c8c24199d2e3d9a7d55ad75efbd008b2
Alfredo <alfredodelafuente@avanzosc.es>
2025-11-04 08:23:08 +0100

    [16.0][ADD] mrp_bom_cost_permission: New group "Show product and BoM cost in printed report".

---
+++ mrp_bom_report_cost_permission/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Mrp Bom Report Cost Permision",
+    "version": "16.0.1.0.0",
+    "category": "Manufacturing/Manufacturing",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "depends": [
+        "mrp",
+    ],
+    "excludes": [],
+    "data": [
+        "security/mrp_spcrp_groups.xml",
+        "report/mrp_report_bom_structure.xml",
+    ],
+    "installable": True,
+}
