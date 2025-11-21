5603c6bf83647e6c1811f25206ba4dee0bf31709
Ane <aneavanzosc@gmail.com>
2025-11-21 10:11:44 +0100

    [16.0][ADD] mrp_workorder_group_block_done: New groups to view "Block" and "Finish" buttons

---
+++ mrp_workorder_group_block_done/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2025 Alfredo de la Fuente - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Mrp Workorder Group Block done",
+    "version": "16.0.1.0.0",
+    "category": "Manufacturing/Manufacturing",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "author": "AvanzOSC",
+    "license": "AGPL-3",
+    "depends": ["mrp", "mrp_usability"],
+    "data": [
+        "security/mrp_workorder_group_block_done.xml",
+        "views/mrp_workorder_views.xml",
+    ],
+    "installable": True,
+}
