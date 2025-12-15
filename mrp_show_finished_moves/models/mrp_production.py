d6bf6d92252a0ae128d4bd8163063f3e79e8a261
luciaavanzosc <luciaavanzosc@gmail.com>
2025-12-15 15:39:08 +0100

    [16.0][IMP] mrp_show_finished_moves: New field last manufactured lot in MOs.

--- mrp_show_finished_moves/models/mrp_production.py
+++ mrp_show_finished_moves/models/mrp_production.py
@@ -1,10 +1,60 @@
 # Copyright 2025 Lucía Echeverría - AvanzOSC
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+import re
+
 from odoo import _, api, fields, models
 
 
 class MrpProduction(models.Model):
     _inherit = "mrp.production"
+
+    last_manufactured_lot = fields.Char(
+        compute="_compute_last_manufactured_lot",
+    )
+
+    @api.depends("product_id")
+    def _compute_last_manufactured_lot(self):
+        MoveLine = self.env["stock.move.line"]
+        for production in self:
+            lot = MoveLine.search(
+                [
+                    ("product_id", "=", production.product_id.id),
+                    ("lot_id", "!=", False),
+                    ("state", "=", "done"),
+                ],
+                order="date desc",
+                limit=1,
+            )
+
+            production.last_manufactured_lot = lot.lot_id.name if lot else False
+
+    def _increment_serial_number(self, serial):
+        if not serial:
+            return None
+        match = re.search(r"(\d+)(?!.*\d)", serial)
+        if not match:
+            return serial
+        last_number = int(match.group(1))
+        incremented_number = last_number + 1
+        parts = serial.rsplit(match.group(1), 1)
+        new_serial = (
+            parts[0]
+            + str(incremented_number).zfill(len(match.group(1)))
+            + (parts[1] if len(parts) > 1 else "")
+        )
+        return new_serial
+
+    def _update_move_next_serial(self):
+        for production in self:
+            if production.last_manufactured_lot:
+                next_serial = self._increment_serial_number(
+                    production.last_manufactured_lot
+                )
+                if next_serial:
+                    moves_to_update = production.move_finished_ids.filtered(
+                        lambda m: m.product_id == production.product_id
+                    )
+                    moves_to_update.write({"next_serial": next_serial})
 
     def action_show_finished_move_lines(self):
         self.ensure_one()
@@ -131,6 +181,13 @@ class MrpProduction(models.Model):
                     wo.qty_producing = quantity_done
                     wo.qty_produced = quantity_done
 
+    @api.model
+    def create(self, vals):
+        production = super().create(vals)
+        if production.last_manufactured_lot:
+            production._update_move_next_serial()
+        return production
+
     def write(self, vals):
         res = super().write(vals)
         if "lot_producing_id" in vals:
@@ -163,6 +220,8 @@ class MrpProduction(models.Model):
                                     )
                                 )
                         move.move_line_ids = ml_cmds
+                if "last_manufactured_lot" in vals:
+                    production._update_move_next_serial()
         return res
 
 
