abe48f2e94fd3bb05d825c285e7f5fa62f17487c
Eñaut Alberdi <enautavanzosc@gmail.com>
2026-01-07 15:20:25 +0100

    [16.0][FIX] mrp_workorder_quant_manual_assign: Adds Stock Quant Manual Assign button

--- mrp_workorder_quant_manual_assign/__manifest__.py
+++ mrp_workorder_quant_manual_assign/__manifest__.py
@@ -1,4 +1,5 @@
 # Copyright 2025 Lucía Echeverría - AvanzOSC
+# Copyright 2026 Eñaut Alberdi - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "MRP Workorder Quant Manual Assign",
