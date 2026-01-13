7eb82d7ce43bb7e7e4272dd631d87295d2669665
luciaavanzosc <luciaavanzosc@gmail.com>
2026-01-13 13:44:55 +0100

    [14.0][ADD] mrp_repair_reserved_quantity: Repair quants after done productions.

---
+++ mrp_repair_reserved_quantity/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2025 Lucía Echeverría - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+{
+    "name": "MRP Repair Reserved Quantity",
+    "summary": "Repair quants after done productions.",
+    "version": "14.0.1.0.0",
+    "category": "Manufacturing",
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "depends": ["mrp", "stock_quant_repair_reserved_quantity"],
+    "installable": True,
+}
