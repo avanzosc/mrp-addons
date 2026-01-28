d4e1c4223dd9f1180e5df28c06982f8bbe0c74a7
Eñaut Alberdi <enautavanzosc@gmail.com>
2026-01-28 14:23:48 +0100

    [14.0][ADD] mrp_production_employee_assign: Assign employees to manufacturing orders from a list view

---
+++ mrp_production_employee_assign/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2026 Eñaut Alberdi - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "MRP Production - Employee Assign",
+    "summary": "Assign employees to manufacturing orders from a list view.",
+    "version": "14.0.1.0.0",
+    "author": "Avanzosc",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "category": "MRP",
+    "depends": [
+        "mrp",
+        "hr",
+    ],
+    "data": [
+        "views/mrp_production_employee_assign_views.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
