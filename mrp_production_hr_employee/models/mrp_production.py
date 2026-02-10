db24757191c1865fcf761e1d67fc7de5690c84d4
Eñaut Alberdi <enautavanzosc@gmail.com>
2026-02-10 15:05:35 +0100

    [14.0][ADD] mrp_production_hr_employee: Assign HR employees to manufacturing orders from a list view

--- mrp_production_hr_employee/models/mrp_production.py
+++ mrp_production_hr_employee/models/mrp_production.py
@@ -12,19 +12,13 @@ class MrpProduction(models.Model):
         string="Employees",
     )
 
-    def action_open_wizard(self):
+    def action_open_form(self):
         self.ensure_one()
         return {
-            "name": "Work Order",
             "type": "ir.actions.act_window",
             "res_model": "mrp.production",
-            "res_id": self.id,
             "view_mode": "form",
-            "views": [
-                (
-                    self.env.ref("mrp.mrp_production_form_view").id,
-                    "form",
-                )
-            ],
+            "view_id": self.env.ref("mrp.mrp_production_form_view").id,
+            "res_id": self.id,
             "target": "current",
         }
