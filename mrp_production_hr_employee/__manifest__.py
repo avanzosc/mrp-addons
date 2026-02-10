e5704aae41a0972dd4990eac8c353be2816a65c4
Ane <aneavanzosc@gmail.com>
2026-02-10 09:33:32 +0100

    [14.0][ADD] mrp_production_hr_employee: Add employee_ids field to mrp.production and wizard button

---
+++ mrp_production_hr_employee/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2026 Eñaut Alberdi - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "MRP Production - HR Employee",
+    "version": "14.0.1.0.0",
+    "author": "Avanzosc",
+    "summary": "Assign HR employees to manufacturing orders from a list view.",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "category": "MRP",
+    "depends": [
+        "mrp",
+        "hr",
+    ],
+    "data": [
+        "views/mrp_production_hr_employee_views.xml",
+    ],
+    "license": "AGPL-3",
+    "installable": True,
+}
