98e3685b33869577bb5d7dc69993df7182fb1d11
Ane <aneavanzosc@gmail.com>
2026-02-11 09:28:50 +0100

    [14.0][ADD] mrp_production_tree_show_buttons: Adds action buttons to the Manufacturing Orders tree view

---
+++ mrp_production_tree_show_buttons/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2026 Eñaut Alberdi - AvanzOSC
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Mrp Production Show Button Unreserve",
+    "version": "14.0.1.1.0",
+    "category": "MRP",
+    "summary": """
+    Show Manufacturing Order action buttons in the mrp.production list view
+    """,
+    "license": "AGPL-3",
+    "author": "AvanzOSC",
+    "website": "https://github.com/avanzosc/mrp-addons",
+    "depends": [
+        "mrp",
+    ],
+    "data": [
+        "views/mrp_production_tree_buttons_view.xml",
+    ],
+    "installable": True,
+}
