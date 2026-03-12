a372ce05b1f20f22a8f3610bca2d3a60c343cd4e
Alfredo <alfredodelafuente@avanzosc.es>
2026-03-12 08:58:14 +0100

    [18.0][MIG] mrp_bom_multiple_production_qty: Migration to 18.0

--- mrp_bom_multiple_production_qty/__manifest__.py
+++ mrp_bom_multiple_production_qty/__manifest__.py
@@ -4,7 +4,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 {
     "name": "MRP Bom Multiple Production Qty",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Manufacturing",
     "author": "Guadaltech Soluciones Tecnológicas S.L., AvanzOSC",
     "website": "https://github.com/avanzosc/mrp-addons",
