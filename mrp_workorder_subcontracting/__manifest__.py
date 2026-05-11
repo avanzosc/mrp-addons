730cd4ef8c65c54b6c2e7defe8cd0c284019dbea
luciaavanzosc <luciaavanzosc@gmail.com>
2026-05-11 14:27:07 +0200

    [18.0][MIG] mrp_workorder_subcontracting: Migration to 18.0.

--- mrp_workorder_subcontracting/__manifest__.py
+++ mrp_workorder_subcontracting/__manifest__.py
@@ -2,7 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Mrp Workorder Subcontracting",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Avanzosc",
     "website": "https://github.com/avanzosc/mrp-addons",
     "category": "Manufacturing/Manufacturing",
